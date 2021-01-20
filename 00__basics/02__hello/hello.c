837de816de82085cad4ccd4120b4d76d76fad84a
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-20 10:39:44 +0100

    basics: add second example

---
+++ 00__basics/02__hello/hello.c
@@ -0,0 +1,34 @@
+/*
+  Hello
+
+  A basic linux kernel module, 2.6.18.
+
+  A "static" declaration for variables and functions is necessary to
+  avoid namespace conflicts with other functions by the same name (in
+  same "common" namespace!).
+
+  ---
+  References:
+  Linux Kernel Module Programming Guide, Peter Jay Salzman, 2007-05-18
+*/
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int __init init_hello(void)
+{
+  printk(KERN_INFO "Hello World!\n");
+  return 0;
+}
+
+static void __exit exit_hello(void)
+{
+  printk(KERN_INFO "Goodbye World!\n");
+}
+
+module_init(init_hello);
+module_exit(exit_hello);
+MODULE_AUTHOR("Lothar Rubusch");
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Hello World");
