e5f2e0d65c8c4aefba0c096401a4a7736ea235f6
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-26 14:04:30 +0100

    basics: hello (02) - update

--- 00__basics/02__hello/hello.c
+++ 00__basics/02__hello/hello.c
@@ -1,31 +1,69 @@
 /*
-  Hello Module
-
-  A basic linux kernel module, 2.6.18.
-
-  A "static" declaration for variables and functions is necessary to
-  avoid namespace conflicts with other functions by the same name (in
-  same "common" namespace!).
-
-  ---
-  References:
-  Linux Kernel Module Programming Guide, Peter Jay Salzman, 2007-05-18
 */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 
-static int __init init_hello(void)
+/*
+  forwards
+*/
+
+static int __init mod_init(void);
+static void __exit mod_exit(void);
+
+int init_hello(void);
+void cleanup_hello(void);
+
+
+/*
+  globals
+*/
+
+///
+
+
+/*
+  implementation
+*/
+
+///
+
+
+/*
+  start / stop module
+*/
+
+int init_hello(void)
 {
 	printk(KERN_INFO "Hello World!\n");
 	return 0;
 }
 
-static void __exit exit_hello(void)
+void cleanup_hello(void)
 {
 	printk(KERN_INFO "Goodbye World!\n");
 }
 
-module_init(init_hello);
-module_exit(exit_hello);
+
+/*
+  init / exit
+*/
+
+static int __init mod_init(void)
+{
+	return init_hello();
+}
+
+static void __exit mod_exit(void)
+{
+	cleanup_hello();
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Lothar Rubusch <l.rubusch@gmail.com>");
+MODULE_DESCRIPTION("Demonstrates Hello World!");
