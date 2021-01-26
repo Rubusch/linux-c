296ebea310f4a8590cff33c7f01cc8269736f4d8
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-26 23:28:58 +0100

    chardev: removed includes

--- 00__basics/chardev__03_devicefile_and_chardev/hellofile.c
+++ 00__basics/chardev__03_devicefile_and_chardev/hellofile.c
@@ -4,10 +4,8 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kdev_t.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
-#include <linux/device.h>
 
 /*
   forwards
