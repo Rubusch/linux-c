4b2825b618650d386578f83c5fd2842304eaa6af
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-28 23:37:17 +0100

    interrupt: static workqueue - include cleanup

--- 00__basics/interrupt__02_workqueue_static/interrupt.c
+++ 00__basics/interrupt__02_workqueue_static/interrupt.c
@@ -43,16 +43,9 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
-#include <linux/kdev_t.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
-#include <linux/device.h>
-#include <linux/slab.h>
-#include <linux/uaccess.h>
 #include <linux/interrupt.h>
-#include <asm/io.h>
-#include <asm/hw_irq.h>
-#include <linux/workqueue.h>
 
 /*
   forwards
