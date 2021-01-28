460086f16b489499634119770949ae41c262f4e0
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-28 09:33:20 +0100

    hello: 03 - adjust email

--- 00__basics/03__hello_license/hello.c
+++ 00__basics/03__hello_license/hello.c
@@ -1,26 +1,12 @@
 /*
-  Hello Module
-
-  A basic linux kernel module, 2.6.18. It demonstrates modinfo
-  support, author, tainting (GPL License), description showed by:
-  # modinfo hello
-
-
-  A "static" declaration for variables and functions is necessary to
-  avoid namespace conflicts with other functions by the same name (in
-  same "common" namespace!).
-
-  ---
-  References:
-  Linux Kernel Module Programming Guide, Peter Jay Salzman, 2007-05-18
 */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 
-#define DRIVER_AUTHOR   "Lothar Rubusch <Lothar.Rubusch@gmx.ch>"
-#define DRIVER_DESC     "a sample module"
+#define DRIVER_AUTHOR   "Lothar Rubusch <l.rubusch@gmail.com>"
+#define DRIVER_DESC     "A sample module."
 
 
 static int __init init_hello(void)
