9a0c26aadc00553173160c1f088531629971e683
Lothar Rubusch <l.rubusch@gmx.ch>
2021-01-28 23:47:35 +0100

    interrupt: dynamic workqueue

---
+++ 00__basics/interrupt__03_workqueue_dynamic/README.md
@@ -0,0 +1,65 @@
+# Hello Interrupt Module
+
+The source was tested compiled and running on 5.4.75.  
+
+
+## Usage
+
+```
+$ make
+$ sudo insmod interrupt.ko
+
+// see interrupt handler registered for interrupt 11
+$ cat /proc/interrupts | grep lothar
+      11:          0          0          0          0          0          0          0          0  IR-IO-APIC   11-edge      lothars_device
+
+// start event
+$ sudo cat /dev/lothars_device
+    Message from syslogd@debian at Jan 28 22:53:07 ...
+    kernel:do_IRQ: 2.56 No irq handler for vector
+
+$ sudo rmmod interrupt
+
+$ dmesg | tail
+    Jan 28 22:52:55 debian kernel: init_hello_interrupt() initializing...
+    Jan 28 22:52:55 debian kernel: init_hello_interrupt() major = 244, minor = 123
+    Jan 28 22:53:07 debian kernel: hello_interrupt_read()
+    Jan 28 22:53:07 debian kernel: do_IRQ: 2.56 No irq handler for vector
+    Jan 28 22:53:37 debian kernel: cleanup_hello_interrupt() READY.
+
+```
+
+NB: the ``vector_irq[]`` is not available right away.
+
+
+## Notes
+
+There are **4 bottom half mechanisms** are available in Linux:  
+
+ * Workqueue
+ * Threaded IRQs
+ * Softirq
+ * Tasklets
+
+
+Work queues are added in the Linux kernel 2.6 version. Work queues are a different form of deferring work. Work queues defer work into a kernel thread; this bottom half always runs in process context. Because workqueue is allowing users to create a kernel thread and bind work to the kernel thread. So, this will run in process context and the work queue can sleep.  
+
+Code deferred to a work queue has all the usual benefits of process context. Most importantly, work queues are schedulable and can therefore sleep.  
+
+Normally, it is easy to decide between using workqueue and softirq/tasklet:  
+
+ * If the deferred work needs to sleep, then workqueue is used.
+ * If the deferred work need not sleep, then softirq or tasklet are used.
+
+There are two ways to implement Workqueue in Linux kernel.  
+
+ * Using global workqueue (Static / Dynamic)
+ * Creating Own workqueue (We will see in the next tutorial)
+
+---
+
+## References:
+
+ * Linux Kernel Module Programming Guide, Peter Jay Salzman, 2007-05-18
+ * Highly inspired by / many thanks to www.embetronicx.com (2021)
+ * https://github.com/Embetronicx/Tutorials/tree/master/Linux/Device_Driver
---
+++ 00__basics/interrupt__03_workqueue_dynamic/interrupt.c
@@ -0,0 +1,296 @@
+/*
+  Triggering Hardware Interrupt through Software
+
+  Intel processors handle interrupt using IDT (Interrupt Descriptor
+  Table).  The IDT consists of 256 entries with each entry
+  corresponding to a vector and of 8 bytes. All the entries are a
+  pointer to the interrupt handling function. The CPU uses IDTR to
+  point to IDT. The relation between those two can be depicted as
+  below,
+
+  IDTR Register
+
+
+   47                               16 15              0
+  +-----------------------------------+-----------------+
+  | IDT Base Addr                     |   IDT Limit     |
+  +-----------------------------------+-----------------+
+                     |                       |
+                     |      +----------------+
+                     |      |
+		     |      |   Interrupt Descriptor Table (IDT)
+		     |      V         +-----------------+
+		     +---->(+)------->| Gate for        |
+		     |                |     Interrupt n |
+		     |                +-----------------+
+		     |                |                 |
+		     |               ...               ...
+		     |                |                 |
+		     |                +-----------------+
+		     |                +-----------------+
+		     |                +-----------------+
+		     |                | Gate Irpt. #2   |
+		     |                +-----------------+
+		     +--------------->| Gate for        |
+		                      |    Interrupt #1 |
+				      +-----------------+
+
+  An interrupt can be programmatically raised using int
+  instruction. For example, the Linux system call was using int $0x80.
+*/
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/interrupt.h>
+#include <asm/io.h>
+#include <asm/hw_irq.h>
+
+/*
+  forwards
+*/
+
+static int __init mod_init(void);
+static void __exit mod_exit(void);
+
+int init_hello_interrupt(void);
+void cleanup_hello_interrupt(void);
+
+// interrupt handler
+static irqreturn_t irq_handler(int, void *);
+
+// chardev read()
+static ssize_t hello_interrupt_read(struct file *, char __user *, size_t, loff_t *);
+
+// workqueue (dynamic)
+static struct work_struct workqueue;
+void workqueue_fn(struct work_struct *work);
+
+
+/*
+  globals
+*/
+
+// chardev device
+#define HELLO_DEVICE_MINOR 123
+
+#define HELLO_DEVICE_CHRDEV "lothars_chrdev"
+#define HELLO_DEVICE_CLASS "lothars_class"
+#define HELLO_DEVICE_NAME "lothars_device"
+
+// chardev
+dev_t dev;
+static struct class *dev_class;
+static struct cdev hello_interrupt_cdev;
+
+static struct file_operations fops =
+{
+	.owner = THIS_MODULE,
+	.read = hello_interrupt_read,
+};
+
+// interrupt request number
+#define IRQ_NO 11
+
+
+/*
+  implementation
+*/
+
+/*
+  chardev read()
+
+  Simulates something similar as an interrupt from a device.
+
+  This used to work on older kernel versions, but fails on later
+  versions. The reason is that the generic IRQ handler do_IRQ() has
+  been changed for better IRQ handling performance. Instead of using
+  the irq_to_desc() function to get the IRQ descriptor, it reads it
+  from the per-CPU data. The descriptor is put there during the
+  physical device initialization. Since this pseudo device driver
+  don't have a physical device, do_IRQ() don't find it there and
+  returns with an error. If we want to simulate IRQ using software
+  interrupt, we must first write the IRQ descriptor to the per-CPU
+  data. Unfortunately, the symbol vector_irq, the array of the IRQ
+  descriptors in the per-CPU data, is not exported to kernel modules
+  during kernel compilation. The only way to change it, is to
+  recompile the whole kernel. If you think it worth the effort, you
+  can add the line:
+
+    EXPORT_SYMBOL (vector_irq);
+
+  in the file: arch/x86/kernel/irq.c right after all the include lines.
+
+  https://stackoverflow.com/questions/57391628/error-while-raising-interrupt-11-with-inline-asm-into-kernel-module
+*/
+static ssize_t hello_interrupt_read(struct file *filp, char __user *buf, size_t len, loff_t *off)
+{
+	struct irq_desc *desc;
+
+	printk( KERN_INFO "%s()", __func__);
+	desc = irq_to_desc(IRQ_NO);
+	if (!desc) {
+		return -EINVAL;
+	}
+
+	/* interrupt trick: issue IRQ11 at READ event on device */
+
+/* // TODO uncomment the following and rebuild your kernel...
+	__this_cpu_write(vector_irq[59], desc); // won't compile
+						// unless 'vector_irq'
+						// was exported in the
+						// kernel and thus
+						// kernel recompiled
+// */
+
+	asm("int $0x38"); // corresponding to IRQ 11
+
+	return 0;
+}
+
+/*
+  interrupt handler
+*/
+static irqreturn_t irq_handler(int irq, void *dev_id)
+{
+	printk(KERN_INFO "%s() shared IRQ: interrupt occured\n", __func__);
+
+	// allocating workqueue
+	schedule_work(&workqueue);
+
+	return IRQ_HANDLED;
+}
+
+/*
+  workqueue
+*/
+void workqueue_fn(struct work_struct *work)
+{
+	printk(KERN_INFO "%s()\n", __func__);
+}
+
+
+/*
+  start / stop module
+*/
+
+int init_hello_interrupt(void)
+{
+	printk(KERN_INFO "%s() initializing...\n", __func__);
+	if (0 > alloc_chrdev_region(&dev, HELLO_DEVICE_MINOR, 1, HELLO_DEVICE_CHRDEV)) {
+		printk(KERN_ERR "alloc_chrdev_region() failed\n");
+		return -ENOMEM;
+	}
+	printk(KERN_INFO "%s() major = %d, minor = %d\n", __func__, MAJOR(dev), MINOR(dev));
+
+	cdev_init(&hello_interrupt_cdev, &fops);
+
+	if (0 > cdev_add(&hello_interrupt_cdev, dev, 1)) {
+		printk(KERN_ERR "cdev_add() failed\n");
+		goto err_cdev;
+	}
+
+	dev_class = class_create(THIS_MODULE, HELLO_DEVICE_CLASS);
+	if (NULL == dev_class) {
+		printk(KERN_ERR "class_create() failed\n");
+		goto err_class;
+	}
+
+	if (NULL == device_create(dev_class, NULL, dev, NULL, HELLO_DEVICE_NAME)) {
+		printk(KERN_ERR "device_create() failed\n");
+		goto err_device;
+	}
+
+	/**
+	 * request_irq - Add a handler for an interrupt line
+	 * @irq:The interrupt line to allocate
+	 * @handler:Function to be called when the IRQ occurs.
+	 *Primary handler for threaded interrupts
+	 *If NULL, the default primary handler is installed
+	 * @flags:Handling flags
+	 * @name:Name of the device generating this interrupt
+	 * @dev:A cookie passed to the handler function
+	 *
+	 * This call allocates an interrupt and establishes a handler; see
+	 * the documentation for request_threaded_irq() for details.
+	 */
+	if (request_irq(IRQ_NO, irq_handler, IRQF_SHARED, HELLO_DEVICE_NAME, (void *)(irq_handler))) {
+		printk(KERN_ERR "request_irq() failed!\n");
+		goto err_irq;
+	}
+
+	INIT_WORK(&workqueue, workqueue_fn);
+
+	return 0;
+
+
+err_device:
+	device_destroy(dev_class, dev);
+
+err_class:
+	class_destroy(dev_class);
+
+err_cdev:
+	cdev_del(&hello_interrupt_cdev);
+	unregister_chrdev_region(dev, 1);
+
+err_irq:
+	free_irq(IRQ_NO, (void *)(irq_handler));
+
+	return -ENOMEM;
+}
+
+void cleanup_hello_interrupt(void)
+{
+	/**
+	 *free_irq - free an interrupt allocated with request_irq
+	 *@irq: Interrupt line to free
+	 *@dev_id: Device identity to free
+	 *
+	 *Remove an interrupt handler. The handler is removed and if the
+	 *interrupt line is no longer in use by any driver it is disabled.
+	 *On a shared IRQ the caller must ensure the interrupt is disabled
+	 *on the card it drives before calling this function. The function
+	 *does not return until any executing interrupts for this IRQ
+	 *have completed.
+	 *
+	 *This function must not be called from interrupt context.
+	 *
+	 *Returns the devname argument passed to request_irq.
+	 */
+	free_irq(IRQ_NO, (void *)(irq_handler));
+
+	// chardev
+	device_destroy(dev_class, dev);
+	class_destroy(dev_class);
+	cdev_del(&hello_interrupt_cdev);
+	unregister_chrdev_region(dev, 1);
+
+	printk(KERN_INFO "%s() READY.\n", __func__);
+}
+
+
+/*
+  init / exit
+*/
+
+static int __init mod_init(void)
+{
+	return init_hello_interrupt();
+}
+
+static void __exit mod_exit(void)
+{
+	cleanup_hello_interrupt();
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Lothar Rubusch <l.rubusch@gmail.com>");
+MODULE_DESCRIPTION("Demonstrates the Hello Interrupt World!");
