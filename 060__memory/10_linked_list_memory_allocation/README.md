92cfcec53470bb7b49c5ffcebf01d79f241e7c97
Lothar Rubusch <l.rubusch@gmail.com>
2023-11-25 18:19:49 +0100

    mem: add memory demo for linked list allocation
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ 060__memory/10_linked_list_memory_allocation/README.md
@@ -0,0 +1,80 @@
+# Linked List Memory Allocation
+
+Allocate a circular linked list in the kernel memory, composed of several notes. Each node will be composed of two variables:  
+1. A `buffer` pointer that points to a memory buffer allocated with `devm_kmalloc()` using a "for" loop  
+2. A `next` pointer that points to the next node of the linked list  
+
+NB: The demo uses a device tree declaration `linked_memory` which is mainly needed to use the miscdevice struture, and device related API.  
+
+# Build
+
+## Devicetree
+
+copy it to the specified location in the linux sources (6.3), then build it  
+```
+$ cd linux
+$ cp -arf <SOURCES>/devicetree/arch ./
+
+$ make dtbs
+  DTC     arch/arm64/boot/dts/broadcom/bcm2710-rpi-3-b.dtb
+```
+Copy the file `bcm2710-rpi-3-b.dtb` to the target overwriting the `/boot/bcm2710-rpi-3-b.dtb`. In case make a safety backup first.  
+
+## Module
+
+Should crosscompile - having crossbuild-essentials-arm64 installed, ARCH, and CROSS_COMPILE set, execute  
+```
+$ cd ./module
+$ make
+```
+Copy the module over to the target  
+
+## Usage
+
+```
+root@ctrl001:/home/pi# insmod ./list_alloc.ko
+root@ctrl001:/home/pi# echo abc > /dev/lothars_dev
+root@ctrl001:/home/pi# echo def > /dev/lothars_dev
+root@ctrl001:/home/pi# echo ghi > /dev/lothars_dev
+root@ctrl001:/home/pi# cat /dev/lothars_dev
+    abc
+    def
+    ghi
+root@ctrl001:/home/pi# rmmod list_alloc
+```
+
+Observe the `/var/log/messages` in parallel  
+```
+Nov 21 10:57:00 ctrl001 kernel: [  304.742042] lothars_probe() - called
+...
+Nov 21 10:57:23 ctrl001 kernel: [  304.742747] lothars_probe() - got minor 121
+Nov 21 10:57:23 ctrl001 kernel: [  328.167236] lothars_dev_open() - called
+Nov 21 10:57:23 ctrl001 kernel: [  328.167597] lothars_dev_write() - called
+Nov 21 10:57:23 ctrl001 kernel: [  328.167622] lothars_dev_write() - node number: 1
+...
+Nov 21 10:57:30 ctrl001 kernel: [  328.167713] lothars_dev_close() - called
+Nov 21 10:57:30 ctrl001 kernel: [  335.227221] lothars_dev_open() - called
+Nov 21 10:57:30 ctrl001 kernel: [  335.227381] lothars_dev_write() - called
+Nov 21 10:57:30 ctrl001 kernel: [  335.227403] lothars_dev_write() - node number: 1
+Nov 21 10:57:30 ctrl001 kernel: [  335.227436] lothars_dev_write() - called
+Nov 21 10:57:30 ctrl001 kernel: [  335.227454] lothars_dev_write() - node number: 2
+...
+Nov 21 10:57:39 ctrl001 kernel: [  335.227541] lothars_dev_close() - called
+Nov 21 10:57:39 ctrl001 kernel: [  344.002294] lothars_dev_open() - called
+Nov 21 10:57:39 ctrl001 kernel: [  344.002446] lothars_dev_write() - called
+Nov 21 10:57:39 ctrl001 kernel: [  344.002467] lothars_dev_write() - node number: 2
+Nov 21 10:57:39 ctrl001 kernel: [  344.002501] lothars_dev_write() - called
+Nov 21 10:57:39 ctrl001 kernel: [  344.002520] lothars_dev_write() - node number: 3
+...
+Nov 21 10:57:50 ctrl001 kernel: [  344.002617] lothars_dev_close() - called
+Nov 21 10:57:50 ctrl001 kernel: [  354.824117] lothars_dev_open() - called
+Nov 21 10:57:50 ctrl001 kernel: [  354.824320] lothars_dev_read() - called
+Nov 21 10:57:50 ctrl001 kernel: [  354.824468] lothars_dev_read() - called
+Nov 21 10:57:50 ctrl001 kernel: [  354.824556] lothars_dev_read() - called
+Nov 21 10:57:50 ctrl001 kernel: [  354.824632] lothars_dev_read() - called
+...
+Nov 21 10:58:04 ctrl001 kernel: [  355.102919] lothars_dev_close() - called
+```
+
+## References
+* Linux Driver Development for Embedded Procesesors, A. L. Rios, 2018, p. 363  
---
+++ 060__memory/10_linked_list_memory_allocation/module/list_alloc.c
@@ -0,0 +1,250 @@
+/*
+  linked list memory allocation
+
+  ---
+  REFERENCES:
+  - Linux Driver Development for Embedded Processors, A. L. Rios, 2018
+*/
+
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/miscdevice.h>
+#include <linux/platform_device.h>
+#include <linux/delay.h>
+#include <linux/of_device.h>  /* struct of_device_id */
+
+static int block_number = 10;
+static int block_size = 5;
+static int size_to_read = 0;
+static int node_count = 1;
+static int cnt = 0;
+
+/* The node definition of the linked list elements */
+typedef struct dnode
+{
+	char *buffer;
+	struct dnode *next;
+} data_node;
+
+/* The list structure to manage the nodes of the linked list */
+typedef struct lnode
+{
+	data_node *head;
+	data_node *cur_write_node;
+	data_node *cur_read_node;
+	int cur_read_offset;
+	int cur_write_offset;
+} listic;
+static listic new_listic;
+
+/* Allocate the linked list elements and memory buffers
+
+   First allocate a first node, then allocate the rest of the linked
+   list nodes up to block_number through a loop.
+ */
+static int create_list(struct platform_device* pdev)
+{
+	data_node *new_node, *prev_node, *head_node;
+	int idx;
+
+	/* allocate the first node */
+	new_node = devm_kmalloc(&pdev->dev, sizeof(data_node), GFP_KERNEL);
+	if (!new_node) {
+		return -ENOMEM;
+	}
+
+	/* allocate the memory buffer of the first node */
+	new_node->buffer = devm_kmalloc(&pdev->dev, sizeof(*(new_node->buffer)) * block_size, GFP_KERNEL);
+	if (!new_node->buffer) {
+		return -ENOMEM;
+	}
+	new_node->next = NULL;
+	new_listic.head = new_node;
+	head_node = new_node;
+	prev_node = new_node;
+
+	/* allocate the rest of the linked list nodes up to
+	   block_number */
+	for (idx = 0; idx < block_number; idx++) {
+		new_node = devm_kmalloc(&pdev->dev, sizeof(data_node), GFP_KERNEL);
+		if (!new_node) {
+			return -ENOMEM;
+		}
+		new_node->buffer = devm_kmalloc(&pdev->dev, sizeof(*(new_node->buffer)) * block_size, GFP_KERNEL);
+		if (!new_node->buffer) {
+			return -ENOMEM;
+		}
+		new_node->next = NULL;
+		prev_node->next = new_node;
+		prev_node = new_node;
+
+	}
+
+	new_node->next = head_node;
+
+	new_listic.cur_read_node = head_node;
+	new_listic.cur_write_node = head_node;
+	new_listic.cur_read_offset = 0;
+	new_listic.cur_write_offset = 0;
+
+	return 0;
+}
+
+static ssize_t
+lothars_dev_write(struct file* file, const char __user* buf, size_t size, loff_t* offset)
+{
+	int size_to_copy;
+
+	pr_info("%s() - called", __func__);
+	pr_info("%s() - node number: %d", __func__, node_count);
+	if ((0 == *offset) || (1 == node_count))
+		size_to_read += size;
+
+	if (size < block_size - new_listic.cur_write_offset)
+		size_to_copy = size;
+	else
+		size_to_copy = block_size - new_listic.cur_write_offset;
+
+	if (copy_from_user(new_listic.cur_write_node->buffer + new_listic.cur_write_offset, buf, size_to_copy) ) {
+		return -EFAULT;
+	}
+
+	*(offset) += size_to_copy;
+	new_listic.cur_write_offset += size_to_copy;
+	if (new_listic.cur_write_offset == block_size) {
+		new_listic.cur_write_node = new_listic.cur_write_node->next;
+		new_listic.cur_write_offset = 0;
+		node_count++;
+		if (block_number < node_count) {
+			new_listic.cur_read_node = new_listic.cur_write_node;
+			new_listic.cur_read_offset = 0;
+			node_count = 1;
+			cnt = 0;
+			size_to_read = 0;
+		}
+	}
+
+	return size_to_copy;
+}
+
+static ssize_t
+lothars_dev_read(struct file* file, char __user* buf, size_t count, loff_t* offset)
+{
+	int size_to_copy, read_value;
+
+	pr_info("%s() - called", __func__);
+	read_value = size_to_read - (block_size * cnt);
+
+	if (*offset < size_to_read) {
+		if (read_value < block_size - new_listic.cur_read_offset)
+			size_to_copy = read_value;
+		else
+			size_to_copy = block_size - new_listic.cur_read_offset;
+
+		if (copy_to_user(buf, new_listic.cur_read_node->buffer + new_listic.cur_read_offset, size_to_copy))
+			return -EFAULT;
+
+		new_listic.cur_read_offset += size_to_copy;
+		(*offset) += size_to_copy;
+
+		if (new_listic.cur_read_offset == block_size) {
+			cnt = cnt + 1;
+			new_listic.cur_read_node = new_listic.cur_read_node->next;
+			new_listic.cur_read_offset = 0;
+		}
+
+		return size_to_copy;
+	} else {
+		msleep(250);
+		new_listic.cur_read_node = new_listic.head;
+		new_listic.cur_write_node = new_listic.head;
+		new_listic.cur_read_offset = 0;
+		new_listic.cur_write_offset = 0;
+		node_count = 1;
+		cnt = 0;
+		size_to_read = 0;
+	}
+
+	// else path reaches here
+	return 0;
+}
+
+static int
+lothars_dev_open(struct inode* inode, struct file* file)
+{
+	pr_info("%s() - called", __func__);
+	return 0;
+}
+
+static int
+lothars_dev_close(struct inode* inode, struct file* file)
+{
+	pr_info("%s() - called", __func__);
+	return 0;
+}
+
+static const struct file_operations lothars_dev_fops = {
+	.open = lothars_dev_open,
+	.read = lothars_dev_read,
+	.write = lothars_dev_write,
+	.release = lothars_dev_close,
+};
+
+static struct miscdevice lothars_miscdevice = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "lothars_dev",
+	.fops = &lothars_dev_fops,
+};
+
+static int
+lothars_probe(struct platform_device *pdev)
+{
+	int ret;
+	pr_info("%s() - called", __func__);
+
+	create_list(pdev);
+	ret = misc_register(&lothars_miscdevice);
+	if (0 != ret) {
+		pr_err("%s() - could not register the miscdevice structure",
+		       __func__);
+		return ret;
+	}
+
+	pr_info("%s() - got minor %i",
+		__func__, lothars_miscdevice.minor);
+
+	return 0;
+}
+
+static int
+lothars_remove(struct platform_device *pdev)
+{
+	pr_info("%s() - called", __func__);
+	misc_deregister(&lothars_miscdevice);
+
+	return 0;
+}
+
+/*
+   For the miscdevice and usage of the device API the DT is needed
+  */
+static const struct of_device_id lothars_of_ids[] = {
+	{ .compatible = "lothars,linked_memory" },
+	{},
+};
+MODULE_DEVICE_TABLE(of, lothars_of_ids);
+
+static struct platform_driver lothars_platform_driver = {
+	.probe = lothars_probe,
+	.remove = lothars_remove,
+	.driver = {
+		.name = "linked_memory",
+		.of_match_table = lothars_of_ids,
+	},
+};
+module_platform_driver(lothars_platform_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Lothar Rubusch <l.rubusch@gmail.com>");
+MODULE_DESCRIPTION("a memory allocation demo");
