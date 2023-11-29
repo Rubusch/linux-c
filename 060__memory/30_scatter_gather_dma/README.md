4c90754d2925754bf719eb44816643ddcca1986b
Lothar Rubusch <l.rubusch@gmx.ch>
2023-11-29 23:11:29 +0000

    dma: add scatterlist / dma demo
    Signed-off-by: Lothar Rubusch <l.rubusch@gmx.ch>

---
+++ 060__memory/30_scatter_gather_dma/README.md
@@ -0,0 +1,138 @@
+# Scatter/Gather DMA Demo
+
+The following post describes IMHO very well, one of the fundamental understanding issues of how the scatterlist concept works.  
+
+Q: How to use locks on DMA channels?  
+
+A: Don't! DMA is fast, blocking is slow. Any blocking approach slows DMA down here. Use the completion mechanism if needed to signal, if an operation finished.  
+
+Q: When to call `dma_sync_{single,sg}()`?  
+
+A: So, firstly, just map it with dma_map_{single,sg}(), and after each DMA transfer call either:  
+```
+dma_sync_single_for_cpu(dev, dma_handle, size, direction);
+```
+or:  
+```
+dma_sync_sg_for_cpu(dev, sglist, nents, direction);
+```
+Then, if you wish to let the device get at the DMA area again, finish accessing the data with the CPU, and then before actually giving the buffer to the hardware call either:  
+```
+dma_sync_single_for_device(dev, dma_handle, size, direction);
+```
+or
+```
+dma_sync_sg_for_device(dev, sglist, nents, direction);
+```
+If you don’t touch the data from the first `dma_map_*()` call till `dma_unmap_*()`, then you don’t have to call the `dma_sync_*()` routines at all.  
+
+Q: What is the difference between DMA-Engine and DMA-Controller?  
+ref: https://stackoverflow.com/questions/44195754/what-is-the-difference-between-dma-engine-and-dma-controller  
+author (answer): https://stackoverflow.com/users/5345652/sharon-katz  
+
+A:   
+DMA - Direct memory access. The operation of your driver reading or writing from/to your HW memory without the CPU being involved in it (freeing it to do other stuff).  
+
+DMA Controller - reading and writing can't be done by magic. if the CPU doesn't do it, we need another HW to do it. Many years ago (at the time of ISA/EISA) it was common to use a shared HW on the motherboard that did this operation. In recent years , each HW has its own DMA HW mechanism. But in all cases this specific HW gets the source address and the destination address and passes the data. Usually triggering an interrupt when done.  
+
+DMA Engine - [...] refer to the SW side that handles the DMA. DMA is a little more complicated than usual I\O since all memory SRC and DST has to be physically present at all times during the DMA operation. If the DST address is swapped to disk, the HW will write to a bad address and the system will crash. This and other aspects of DMA are handled by the driver with code sections you probably refer to as the "DMA Engine"   
+
+
+Q: As we know scatter list gathers the memory scattered physically across the memory, but virtually contiguous though. When communicating with DMA it provides an abstracted view of memory to DMA as of this is memory contiguous physically.  
+How does scatterlist handle this? Is it a kind of linked list maintained within the scatterlist?  
+For example if have 4000 bytes of data to be transferred using DMA ,as data is physically contiguous how scatter list makes it contiguous ? will scatterlist implementation allocate 4000 bytes of data using Kmalloc to make sure that it gets a physically contiguous memory? or will it create a linked list?  
+ref: https://stackoverflow.com/questions/29270447/how-does-scatterlist-works-in-linux  
+author (answer): https://stackoverflow.com/users/2511795/0andriy  
+
+A: It seems you are mixing two different scatter lists, i.e. struct scatterlist in Linux kernel and scatter list that might be supported by the specific DMA controller. In both cases they are not contiguous. This is like an array in the first case and linked list in the second. When you call dmaengine_prep_slave_sg() the actual DMA controller driver converts it to the internal structure which might be the copy of the SG list, a new SG list that fits in the maximum DMA block size boundaries, the DMA HW linked list and so on. Depending on the DMA controller driver (let's consider recently added drivers/dma/hsu/hsu.c) the initial SG list is copied and supplied to the hardware in chunks by at most 4 DMA HW descriptors, after finish of each block it gets an interrupt and continues if the initial amount of items is more than 4 until everything is sent / received.  
+
+#### Example
+
+Let's assume we have the following in the SG list that is supplied to DMA controller on the system where the maximum size of DMA block is 1024 (this is usually set by dma_set_max_seg_size() for callers to know about it) and memory page size is 4096 (to be sure that we have contiguous data when the size is less than a page):  
+
+|	| Virtual address |	Length |
+|---|-----------------|--------|
+| 1 |          0xa000 |  0x555 |
+| 2 |          0x7000 |  0x111 |
+
+This, due to DMA controller constraints, become...  
+
+|   | Virtual address | Length |
+|---|-----------------|--------|
+| 1 |          0xa000 |  0x400 |
+| 2 |          0xa400 |  0x155 |
+| 3 |          0x7000 |  0x111 |
+
+...either on the caller (user of DMA controller) or callee (DMA controller itself). On top of that for the blocks 2 and 3 DMA controller driver may apply additional alignment rules, such as 4-byte boundary for each burst. With that it becomes...  
+
+|   | Virtual address | Length |
+|---|-----------------|--------|
+| 1 |          0xa000 |  0x400 |
+| 2 |          0xa400 |  0x154 |
+| 3 |          0xa450 |  0x001 |
+| 4 |          0x7000 |  0x110 |
+| 5 |          0x7110 |  0x001 |
+
+...and DMA controller will service the 5 separate data blocks.  
+
+# Build
+
+## Devicetree
+
+Copy it to the specified location in the linux sources (6.3), then build it  
+```
+$ cd linux
+$ cp -arf <SOURCES>/devicetree/arch ./
+
+$ make dtbs
+  DTC     arch/arm64/boot/dts/broadcom/bcm2710-rpi-3-b.dtb
+```
+Copy the file `bcm2710-rpi-3-b.dtb` to the target overwriting the `/boot/bcm2710-rpi-3-b.dtb`. In case make a safety backup first.  
+
+## Module
+
+Should crosscompile - having crossbuild-essentials-arm64 installed, ARCH, and CROSS_COMPILE set, execute  
+```
+$ cd ./module
+$ make
+```
+Copy the module over to the target  
+
+## Usage
+
+```
+root@ctrl001:/home/pi# insmod ./dma_demo.ko 
+root@ctrl001:/home/pi# echo 123 > /dev/sdma_test 
+root@ctrl001:/home/pi# rmmod dma_demo
+```
+watch the log  
+```
+Nov 29 22:59:30 ctrl001 kernel: [  733.838605] smda_m2m soc:sdma_m2m: lothars_probe() - called
+Nov 29 22:59:30 ctrl001 kernel: [  733.839184] smda_m2m soc:sdma_m2m: lothars_probe() - done
+
+Nov 29 22:59:33 ctrl001 kernel: [  736.631104] sdma_write() - called
+Nov 29 22:59:33 ctrl001 kernel: [  736.631147] smda_m2m soc:sdma_m2m: sdma_write() - the dma_buf_src string is '123
+Nov 29 22:59:33 ctrl001 kernel: [  736.631147] '
+Nov 29 22:59:33 ctrl001 kernel: [  736.631177] smda_m2m soc:sdma_m2m: sdma_write() - scatterlist setup
+Nov 29 22:59:33 ctrl001 kernel: [  736.631202] smda_m2m soc:sdma_m2m: sdma_write() - mapping
+Nov 29 22:59:33 ctrl001 kernel: [  736.631307] smda_m2m soc:sdma_m2m: sdma_write() - init completion
+Nov 29 22:59:33 ctrl001 kernel: [  736.631332] smda_m2m soc:sdma_m2m: sdma_write() - call dmaengine_submit()
+Nov 29 22:59:33 ctrl001 kernel: [  736.631360] smda_m2m soc:sdma_m2m: sdma_write() - call dma_async_issue_pending()
+Nov 29 22:59:33 ctrl001 kernel: [  736.631577] smda_m2m soc:sdma_m2m: dma_sg_callback() - finished SG DMA transaction
+Nov 29 22:59:33 ctrl001 kernel: [  736.631694] smda_m2m soc:sdma_m2m: sdma_write() - the device can read '123
+Nov 29 22:59:33 ctrl001 kernel: [  736.631694] '
+Nov 29 22:59:33 ctrl001 kernel: [  736.631723] sdma_write() - buffer coherent sg copy successful
+
+Nov 29 22:59:53 ctrl001 kernel: [  736.631741] sdma_write() - dma_buf_src is '123
+Nov 29 22:59:53 ctrl001 kernel: [  736.631741] '
+Nov 29 22:59:53 ctrl001 kernel: [  756.679095] smda_m2m soc:sdma_m2m: lothars_remove() - called
+Nov 29 22:59:53 ctrl001 kernel: [  756.679768] smda_m2m soc:sdma_m2m: lothars_remove() - done
+```
+
+## References
+* https://www.kernel.org/doc/html/v6.3/core-api/dma-api-howto.html
+* https://static.lwn.net/kerneldoc/driver-api/dmaengine/client.html
+* https://stackoverflow.com
+* https://github.com/fandahao17/Raspberry-Pi-DMA-Tutorial
+* https://docs.kernel.org/driver-api/dmaengine/dmatest.html
+* https://iosoft.blog/2020/05/25/raspberry-pi-dma-programming/
---
+++ 060__memory/30_scatter_gather_dma/module/dma_demo.c
@@ -0,0 +1,302 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+  Scatter/Gather DMA Demo
+
+  ---
+  REFERENCES:
+  - kernel.org / documentation
+  - https://forums.raspberrypi.com/viewtopic.php?t=130160
+*/
+
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/dma-mapping.h> /* DMA mapping functions */
+
+/*
+  functions needed to allocate a DMA slave channel, set slave and
+  controller specific parameters, get a desriptor for transaction,
+  submit the transaction , issue pending requests and wait for
+  callback notification
+ */
+#include <linux/dmaengine.h>
+#include <linux/miscdevice.h>
+#include <linux/platform_device.h>
+
+#include <linux/of_device.h>  /* struct of_device_id */
+
+// the device simulation
+static char *dma_buf_src;
+static char *dma_buf_dst;
+static struct scatterlist sg_device[1];
+
+struct dma_private
+{
+	struct miscdevice dma_misc_device;
+	struct device *dev;
+	struct scatterlist sg_cpu[1];
+	struct dma_chan *dma_m2m_chan;
+	struct completion dma_m2m_ok;
+};
+
+#define SDMA_BUF_SIZE 4096
+
+static void
+dma_sg_callback(void* data)
+{
+	struct dma_private *dma_priv = data;
+	dev_info(dma_priv->dev, "%s() - finished SG DMA transaction",
+		 __func__);
+	complete(&dma_priv->dma_m2m_ok);
+}
+
+/*
+  This function is a total DMA mock - usually here we would connect
+  DMA_DEV_TO_MEM or DMA_MEM_TO_DEV channels, in our case we have
+  rather something like DMA_MEM_TO_MEM, and even nor particular
+  dmaengine implementation.
+
+  To "mock" the dmaengine, here just copy over as if it were a single
+  mapping.
+*/
+struct dma_async_tx_descriptor*
+lothars_prep_slave_sg( struct dma_chan *chan, struct scatterlist *sgl,
+		       unsigned int sg_len,
+		       enum dma_transfer_direction dir, unsigned long flags)
+{
+	dma_addr_t dma_src = 0;
+	dma_addr_t dma_dst = 0;
+
+	if (!chan || !chan->device || !chan->device->device_prep_slave_sg)
+		return NULL;
+
+	dma_src = sg_dma_address(sgl);
+
+	// HACK: device simulation
+	dma_dst = sg_dma_address(sg_device);
+
+	return chan->device->device_prep_dma_memcpy(chan, dma_dst,
+						    dma_src, SDMA_BUF_SIZE,
+						    flags);
+}
+
+/*
+  Initialize scatterlist arrays with sg_init_table() and set each
+  entry in the allocated arrays with sg_set_buf() to point at given
+  data. The exact DMA direction should be provided: DMA_MEM_TO_MEM.
+  Map the scatterlist to get the DMA addresses by using the
+  dma_map_sg().
+
+  Get the channel descriptors, add DMA operations to the pending
+  queue, issue pending DMA requests and set a wait for callback
+  notifications. After each DMA transaction, compare the values of the
+  transmission and reception buffers.
+ */
+static ssize_t
+sdma_write(struct file* file, const char __user* buf, size_t count, loff_t* offset)
+{
+	unsigned int sg_len = 1, n_sg_cpu = 0, n_sg_device = 0;
+	struct dma_async_tx_descriptor *dma_m2m_desc;
+	struct dma_device *dma_dev;
+	struct dma_private *dma_priv;
+	struct device *dev;
+	dma_cookie_t cookie;
+
+	pr_info("%s() - called", __func__);
+
+	// obtain the private data structure using container_of()
+	dma_priv = container_of(file->private_data,
+			       struct dma_private,
+			       dma_misc_device);
+
+	// obtain the dma_device
+	dma_dev = dma_priv->dma_m2m_chan->device;
+
+	if (copy_from_user(dma_buf_src, buf, count))
+		return -EFAULT;
+
+	dev = dma_priv->dev;
+	dev_info(dev, "%s() - the dma_buf_src string is '%s'",
+		 __func__, dma_buf_src);
+
+
+	// scatterlist
+	dev_info(dev, "%s() - scatterlist setup", __func__);
+        // NB: dma_buf_src should be coherent (TODO verify)
+	sg_init_table(dma_priv->sg_cpu, sg_len);
+	sg_set_buf(dma_priv->sg_cpu, dma_buf_src, SDMA_BUF_SIZE);
+
+	sg_init_table(sg_device, sg_len);
+	sg_set_buf(sg_device, dma_buf_dst, SDMA_BUF_SIZE);
+
+
+	// mapping
+	dev_info(dev, "%s() - mapping", __func__);
+	// NB: our issue here: "slave" is usually a piece of dma hardware!
+	// in M2M dma, this won't be a device, and later down neither a "slave"
+	n_sg_cpu = dma_map_sg(dev, dma_priv->sg_cpu, sg_len, 0);// DMA_TO_DEVICE);
+	if (0 == n_sg_cpu) {
+		dev_err(dev, "%s() - mapping sg_cpu to dma failed", __func__);
+		return -EINVAL;
+	}
+
+	n_sg_device = dma_map_sg(dev, sg_device, sg_len, 0); //DMA_FROM_DEVICE);
+	if (0 == n_sg_device) {
+		dev_err(dev, "%s() - mapping sg_device to dma failed", __func__);
+		return -EINVAL;
+	}
+
+	// operation
+
+        // NB: mock implementation of dmaengine_prep_slave_sg()
+	// "slave" refers to dma hardware, since this is just
+	// a small scatterlist / dma demo, this function here will
+	// be mocked
+	dma_m2m_desc = lothars_prep_slave_sg(
+		dma_priv->dma_m2m_chan, dma_priv->sg_cpu, sg_len,
+		DMA_MEM_TO_MEM, DMA_PREP_INTERRUPT | DMA_CTRL_ACK);
+	if (!dma_m2m_desc) {
+		dev_err(dev, "%s() - m2m operation failed", __func__);
+		return -ENOMEM;
+	}
+
+	dev_info(dev, "%s() - init completion", __func__);
+	dma_m2m_desc->callback = dma_sg_callback;
+	dma_m2m_desc->callback_param = dma_priv;
+	init_completion(&dma_priv->dma_m2m_ok);
+
+	// dmaengine_submit()
+	dev_info(dev, "%s() - call dmaengine_submit()", __func__);
+	cookie = dmaengine_submit(dma_m2m_desc);
+	if (dma_submit_error(cookie)) {
+		dev_err(dev, "%s() - failed to submit DMA", __func__);
+		return -EINVAL;
+	}
+
+	// dma_async_issue_pending() - finish up
+	dev_info(dev, "%s() - call dma_async_issue_pending()", __func__);
+	dma_async_issue_pending(dma_priv->dma_m2m_chan);
+	wait_for_completion(&dma_priv->dma_m2m_ok);
+	dma_async_is_tx_complete(dma_priv->dma_m2m_chan, cookie, NULL, NULL);
+
+	dev_info(dev, "%s() - the device can read '%s'",
+		 __func__, dma_buf_dst);
+
+	// unmap scatterlist
+	dma_unmap_sg(dev, dma_priv->sg_cpu, sg_len, DMA_TO_DEVICE);
+	dma_unmap_sg(dev, sg_device, sg_len, DMA_TO_DEVICE);
+
+
+	if (*(dma_buf_src) != *(dma_buf_dst)) {
+		pr_info("%s() - buffer copy failed", __func__);
+		return -EINVAL;
+	}
+	pr_info("%s() - buffer coherent sg copy successful", __func__);
+	pr_info("%s() - dma_buf_src is '%s'", __func__, dma_buf_src);
+
+	return count;
+}
+
+struct file_operations dma_fops = {
+	write: sdma_write,
+};
+
+static int
+lothars_probe(struct platform_device *pdev)
+{
+	int ret;
+	struct dma_private *dma_priv;
+	dma_cap_mask_t dma_m2m_mask;
+	struct dma_slave_config dma_m2m_config = {0};
+	struct device *dev = &pdev->dev;
+
+	dev_info(dev, "%s() - called", __func__);
+
+	dma_priv = devm_kzalloc(&pdev->dev, sizeof(*dma_priv), GFP_KERNEL);
+	if (!dma_priv) {
+		dev_err(dev, "%s() - allocating dma_riv failed", __func__);
+		return -ENOMEM;
+	}
+
+	dma_priv->dma_misc_device.minor = MISC_DYNAMIC_MINOR;
+	dma_priv->dma_misc_device.name = "sdma_test";
+	dma_priv->dma_misc_device.fops = &dma_fops;
+
+	dma_priv->dev = &pdev->dev;
+
+	// NB: the kzalloc() is sufficient for dma_buf_src, if
+	// dma was already mapped and we had some dma_addr_t values
+	// here, we could use dma_alloc_coherent() and
+	// dma_free_coherent() in the remove() function
+	dma_buf_src = devm_kzalloc(&pdev->dev, SDMA_BUF_SIZE, GFP_ATOMIC);
+	if (!dma_buf_src) {
+		dev_err(dev, "%s() - failed allocating wbuf", __func__);
+		return -ENOMEM;
+	}
+
+	dma_buf_dst = devm_kzalloc(&pdev->dev, SDMA_BUF_SIZE, GFP_ATOMIC);
+	if (!dma_buf_dst) {
+		dev_err(dev, "%s() - failed allocating dma_buf_dst", __func__);
+		return -ENOMEM;
+	}
+
+	dma_cap_zero(dma_m2m_mask);
+	dma_cap_set(DMA_MEMCPY, dma_m2m_mask);
+
+	// NB: instead use here dma_request_slave_channel(<dev>, <name>);
+	dma_priv->dma_m2m_chan = dma_request_channel(dma_m2m_mask, 0, NULL);
+	if (!dma_priv->dma_m2m_chan) {
+		dev_err(dev, "%s() - opening the DMA channel failed", __func__);
+		return -EINVAL;
+	}
+
+	// register miscdevice
+	ret = misc_register(&dma_priv->dma_misc_device);
+	platform_set_drvdata(pdev, dma_priv);
+	dev_info(dev, "%s() - done", __func__);
+
+	// setup config and init dma slaves
+	// (in case of a real dma device involved, this slave config
+	// should be prepared)
+	dma_m2m_config.direction = DMA_MEM_TO_MEM;
+	dma_m2m_config.dst_addr_width = DMA_SLAVE_BUSWIDTH_4_BYTES;
+	ret = dmaengine_slave_config(dma_priv->dma_m2m_chan, &dma_m2m_config);
+	if (ret) {
+		dev_err(dev, "%s() - failed to set slave configuration: %d\n",
+			__func__, ret);
+		return -EINVAL;
+	}
+
+	return ret;
+}
+
+static int
+lothars_remove(struct platform_device *pdev)
+{
+	struct dma_private *dma_priv = platform_get_drvdata(pdev);
+	dev_info(dma_priv->dev, "%s() - called", __func__);
+	misc_deregister(&dma_priv->dma_misc_device);
+	dma_release_channel(dma_priv->dma_m2m_chan);
+	dev_info(dma_priv->dev, "%s() - done", __func__);
+	return 0;
+}
+
+static const struct of_device_id lothars_of_ids[] = {
+	{ .compatible = "lothars,sdma_m2m" },
+	{},
+};
+MODULE_DEVICE_TABLE(of, lothars_of_ids);
+
+static struct platform_driver lothars_driver = {
+	.probe = lothars_probe,
+	.remove = lothars_remove,
+	.driver = {
+		.name = "smda_m2m",
+		.of_match_table = lothars_of_ids,
+	}
+};
+module_platform_driver(lothars_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Lothar Rubusch <l.rubusch@gmail.com>");
+MODULE_DESCRIPTION("a scatter/gather DMA demo");
