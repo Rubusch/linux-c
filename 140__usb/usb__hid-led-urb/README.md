5d79e5720456039a025b17a621be2c59159678ea
Lothar Rubusch <l.rubusch@gmx.ch>
2024-01-29 10:39:06 +0000

    usb: demo renamed
    Signed-off-by: Lothar Rubusch <l.rubusch@gmx.ch>

---
+++ 140__usb/usb__hid-led-urb/README.md
@@ -0,0 +1,131 @@
+# PIC32MX470: usb led demo with switch
+
+Kernel Config: make sure the following is set:  
+- `CONFIG_HID_SUPPORT`
+- `CONFIG_HID_GENERIC`
+
+The communication between the host and the devie here is done asynchronously by using USB request blocks (urbs).  
+
+## Hardware: Microchip Curiosity PIC32MX470 (PIC32MX470512H)
+
+PIC32 Board:  
+https://www.microchip.com/DevelopmentTools/ProductDetails/dm320103
+
+Connection:  
+Connect the USB Micro-B port (J12) of the PIC32MX470 Curiosity
+Development Board to the J19 USB-B type C connector. In case this will
+need a USB type-C male to micro-B male cable.  
+
+TODO    
+
+
+## Software: MPLAB X IDE
+
+MPLAB X IDE: https://www.microchip.com/en-us/tools-resources/develop/mplab-x-ide#tabs
+
+```
+$ sudo apt install -y libusb-1.0-0
+
+$ tar xf ./MPLABX-v6.15-linux-installer.tar
+$ sudo ./MPLABX-v6.15-linux-installer.sh
+-> only select MPLAB X IDE, and
+-> PIC32 support
+```
+
+## Setup the HID Application in the MPLAB IDE
+
+TODO       
+
+# Build
+
+## Module
+
+Having crossbuild-essentials-arm64 installed, `ARCH`, and `CROSS_COMPILE` set, execute  
+```
+$ cd ./module__ltc2607-dual-dac
+$ make
+```
+Copy the module over to the target  
+
+## Usage
+
+```
+# insmod ./pic32mx470-led.ko
+#
+# echo 1 > /sys/bus/usb/devices/1-1.4\:1.0/led
+#
+# echo 1 > /sys/bus/usb/devices/1-1.4\:1.0/led
+# echo 2 > /sys/bus/usb/devices/1-1.4\:1.0/led
+# echo 3 > /sys/bus/usb/devices/1-1.4\:1.0/led
+#
+# echo 0 > /sys/bus/usb/devices/1-1.4\:1.0/led
+# echo 0 > /sys/bus/usb/devices/1-1.4\:1.0/led
+# echo 0 > /sys/bus/usb/devices/1-1.4\:1.0/led
+#
+# rmmod pic32mx470-led.ko
+```
+
+Logs   
+```
+Jan 28 08:19:55 ctrl001 kernel: [27635.731001] usbcore: registered new interface driver lothars_usbled
+
+Jan 28 08:19:59 ctrl001 kernel: [27639.415942] usb 1-1.4: new full-speed USB device number 24 using dwc_otg
+Jan 28 08:19:59 ctrl001 kernel: [27639.518941] usb 1-1.4: New USB device found, idVendor=04d8, idProduct=003f, bcdDevice= 1.00
+Jan 28 08:19:59 ctrl001 kernel: [27639.518969] usb 1-1.4: New USB device strings: Mfr=1, Product=2, SerialNumber=0
+Jan 28 08:19:59 ctrl001 kernel: [27639.518983] usb 1-1.4: Product: Simple HID Device Demo
+Jan 28 08:19:59 ctrl001 kernel: [27639.518996] usb 1-1.4: Manufacturer: Lothars USB HID Demo
+Jan 28 08:19:59 ctrl001 kernel: [27639.520165] lothars_usbled 1-1.4:1.0: usbled_probe() - called
+Jan 28 08:19:59 ctrl001 kernel: [27639.520193] lothars_usbled 1-1.4:1.0: usbled_probe() - usb_endpoint_num() = 1 [1] - endpoint number
+Jan 28 08:19:59 ctrl001 kernel: [27639.520207] lothars_usbled 1-1.4:1.0: usbled_probe() - usb_endpoint_maxp() = 64 [?] - endpoint size
+Jan 28 08:19:59 ctrl001 kernel: [27639.520222] lothars_usbled 1-1.4:1.0: usbled_probe() - endpoint IN address is 0x81
+Jan 28 08:19:59 ctrl001 kernel: [27639.520235] lothars_usbled 1-1.4:1.0: usbled_probe() - endpoint OUT address is 0x01
+Jan 28 08:19:59 ctrl001 kernel: [27639.520280] lothars_usbled 1-1.4:1.0: usbled_probe() - interrupt_in_urb submitted
+Jan 28 08:19:59 ctrl001 mtp-probe: checking bus 1, device 24: "/sys/devices/platform/soc/3f980000.usb/usb1/1-1/1-1.4"
+Jan 28 08:19:59 ctrl001 mtp-probe: bus: 1, device: 24 was not an MTP device
+Jan 28 08:20:00 ctrl001 mtp-probe: checking bus 1, device 24: "/sys/devices/platform/soc/3f980000.usb/usb1/1-1/1-1.4"
+Jan 28 08:20:00 ctrl001 mtp-probe: bus: 1, device: 24 was not an MTP device
+
+Jan 28 08:20:12 ctrl001 kernel: [27652.121522] lothars_usbled 1-1.4:1.0: led_store() - called
+Jan 28 08:20:12 ctrl001 kernel: [27652.121573] lothars_usbled 1-1.4:1.0: led_store() - led = 1
+Jan 28 08:20:12 ctrl001 kernel: [27652.186080] usb 1-1.4: led_urb_out_callback() - called
+
+Jan 28 08:20:16 ctrl001 kernel: [27656.401945] lothars_usbled 1-1.4:1.0: led_store() - called
+Jan 28 08:20:16 ctrl001 kernel: [27656.401996] lothars_usbled 1-1.4:1.0: led_store() - led = 1
+Jan 28 08:20:16 ctrl001 kernel: [27656.467085] usb 1-1.4: led_urb_out_callback() - called
+
+Jan 28 08:20:21 ctrl001 kernel: [27661.459139] lothars_usbled 1-1.4:1.0: led_store() - called
+Jan 28 08:20:21 ctrl001 kernel: [27661.459192] lothars_usbled 1-1.4:1.0: led_store() - led = 2
+Jan 28 08:20:21 ctrl001 kernel: [27661.524085] usb 1-1.4: led_urb_out_callback() - called
+
+Jan 28 08:20:27 ctrl001 kernel: [27667.542249] lothars_usbled 1-1.4:1.0: led_store() - called
+Jan 28 08:20:27 ctrl001 kernel: [27667.542305] lothars_usbled 1-1.4:1.0: led_store() - led = 3
+Jan 28 08:20:27 ctrl001 kernel: [27667.607087] usb 1-1.4: led_urb_out_callback() - called
+```
+Press and release the switch S1, and check the logs   
+```
+Jan 28 08:20:36 ctrl001 kernel: [27676.295224] lothars_usbled 1-1.4:1.0: led_store() - called
+Jan 28 08:20:36 ctrl001 kernel: [27676.295274] lothars_usbled 1-1.4:1.0: led_store() - read status
+Jan 28 08:20:36 ctrl001 kernel: [27676.360084] usb 1-1.4: led_urb_out_callback() - called
+Jan 28 08:20:36 ctrl001 kernel: [27676.385080] usb 1-1.4: led_urb_in_callback() - called
+Jan 28 08:20:36 ctrl001 kernel: [27676.385143] usb 1-1.4: led_urb_in_callback() - switch is OFF
+
+Jan 28 08:20:42 ctrl001 kernel: [27682.118680] lothars_usbled 1-1.4:1.0: led_store() - called
+Jan 28 08:20:42 ctrl001 kernel: [27682.118730] lothars_usbled 1-1.4:1.0: led_store() - read status
+Jan 28 08:20:42 ctrl001 kernel: [27682.183073] usb 1-1.4: led_urb_out_callback() - called
+Jan 28 08:20:42 ctrl001 kernel: [27682.210080] usb 1-1.4: led_urb_in_callback() - called
+Jan 28 08:20:42 ctrl001 kernel: [27682.210147] usb 1-1.4: led_urb_in_callback() - switch is ON
+
+Jan 28 08:20:49 ctrl001 kernel: [27689.811471] lothars_usbled 1-1.4:1.0: led_store() - called
+Jan 28 08:20:49 ctrl001 kernel: [27689.811523] lothars_usbled 1-1.4:1.0: led_store() - read status
+Jan 28 08:20:49 ctrl001 kernel: [27689.876078] usb 1-1.4: led_urb_out_callback() - called
+Jan 28 08:20:49 ctrl001 kernel: [27689.891090] usb 1-1.4: led_urb_in_callback() - called
+Jan 28 08:20:49 ctrl001 kernel: [27689.891188] usb 1-1.4: led_urb_in_callback() - switch is OFF
+Jan 28 08:21:07 ctrl001 kernel: [27707.484278] usbcore: deregistering interface driver lothars_usbled
+Jan 28 08:21:07 ctrl001 kernel: [27707.484387] usb 1-1.4: led_urb_in_callback() - called
+Jan 28 08:21:07 ctrl001 kernel: [27707.484399] usb 1-1.4: led_urb_in_callback() - switch is OFF
+Jan 28 08:21:07 ctrl001 kernel: [27707.484440] lothars_usbled 1-1.4:1.0: usbled_disconnect() - called
+Jan 28 08:21:07 ctrl001 kernel: [27707.484459] lothars_usbled 1-1.4:1.0: usbled_disconnect() - usb led is now disconnected
+```
+
+## References
+* Linux Driver Development for Embedded Procesesors, A. L. Rios, 2018, p. 617ff, p. 628  
---
+++ 140__usb/usb__hid-led-urb/module/pic32mx470-led.c
@@ -0,0 +1,303 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+  USB led demo for the PIC32MX470 with switch button
+
+  The communication between the host and the device is done
+  asynchronously by using USB Request Blocks (URBs)
+ */
+
+#include <linux/slab.h>
+#include <linux/module.h>
+#include <linux/usb.h>
+
+#define USBLED_VENDOR_ID   0x04D8
+#define USBLED_PRODUCT_ID  0x003F
+
+static void led_urb_out_callback(struct urb *urb);
+static void led_urb_in_callback(struct urb *urb);
+
+/*
+  NB: The vendor and product id will only match, if the usbhid module
+   is not present!
+ */
+static const struct usb_device_id usbled_id_table[] = {
+	{ USB_DEVICE(USBLED_VENDOR_ID, USBLED_PRODUCT_ID) },
+	{ },
+};
+MODULE_DEVICE_TABLE(usb, usbled_id_table);
+
+/*
+  Create a private structure that will store the driver's data.
+ */
+struct usb_led {
+	struct usb_device *usbdev;
+	struct usb_interface *intf;
+	struct urb *interrupt_out_urb;
+	struct urb *interrupt_in_urb;
+	struct usb_endpoint_descriptor *interrupt_out_endpoint;
+	struct usb_endpoint_descriptor *interrupt_in_endpoint;
+	u8 irq_data;
+	u8 led_number;
+	u8 ibuf;
+	int interrupt_out_interval;
+	int ep_in;
+	int ep_out;
+};
+
+/*
+ */
+static ssize_t
+led_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct usb_interface *intf = to_usb_interface(dev);
+	struct usb_led *led = usb_get_intfdata(intf);
+
+	dev_info(dev, "%s() - called", __func__);
+
+	return sprintf(buf, "%d\n", led->led_number);
+}
+
+static ssize_t
+led_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	struct usb_interface *intf = to_usb_interface(dev);
+	struct usb_led *led = usb_get_intfdata(intf);
+	u8 val;
+	int error, ret;
+
+	dev_info(dev, "%s() - called", __func__);
+
+	// transform char array to u8 value
+	error = kstrtou8(buf, 10, &val);
+	if (error)
+		return error;
+
+	led->led_number = val;
+	led->irq_data = val;
+
+	if (val == 0) {
+		dev_info(dev, "%s() - read status", __func__);
+	} else if (val == 1 || val == 2 || val == 3) {
+		dev_info(dev, "%s() - led = %d\n",
+			 __func__, led->led_number);
+	} else {
+		dev_info(dev, "%s() - unknown led %d\n",
+			 __func__, led->led_number);
+		ret = -EINVAL;
+		return ret;
+	}
+
+	// send out
+	ret = usb_submit_urb(led->interrupt_out_urb, GFP_KERNEL);
+	if (ret) {
+		dev_err(dev, "%s() - interrupt_out_urb() failed",
+			__func__);
+		return ret;
+	}
+	return count;
+}
+static DEVICE_ATTR_RW(led);
+
+static void
+led_urb_out_callback(struct urb *urb)
+{
+	struct usb_led *led = urb->context;
+	struct device *dev = &led->usbdev->dev;
+
+	dev_info(dev, "%s() - called", __func__);
+
+	// sync/async unlink faults aren't errors
+	if (urb->status) {
+		if (!(urb->status == -ENOENT ||
+		    urb->status == -ECONNRESET ||
+		    urb->status == -ESHUTDOWN)) {
+			dev_err(dev, "%s() - nonzero write status received: %d",
+				__func__, urb->status);
+		}
+	}
+}
+
+static void
+led_urb_in_callback(struct urb* urb)
+{
+	struct usb_led *led = urb->context;
+	struct device *dev = &led->usbdev->dev;
+	int ret;
+
+	dev_info(dev, "%s() - called", __func__);
+
+	if (urb->status) {
+		if (!(urb->status == -ENOENT ||
+		      urb->status == -ECONNRESET ||
+		      urb->status == -ESHUTDOWN)) {
+			dev_err(dev, "%s() - nonzero write status received: %d",
+				__func__, urb->status);
+		}
+	}
+
+	switch (led->ibuf) {
+	case 0x00:
+		dev_info(dev, "%s() - switch is OFF", __func__);
+		break;
+	case 0x01:
+		dev_info(dev, "%s() - switch is ON", __func__);
+		break;
+	default:
+		dev_info(dev, "%s() - switch has BAD VALUE", __func__);
+	}
+
+	ret = usb_submit_urb(led->interrupt_in_urb, GFP_KERNEL);
+	if (ret) {
+		dev_err(dev, "%s() - failed to submit interrupt_in_urb %d",
+			__func__, ret);
+	}
+}
+
+static int
+usbled_probe(struct usb_interface *interface, const struct usb_device_id *id)
+{
+	// obtain the struct usb_device from the usb_interface
+	struct usb_device *usbdev = interface_to_usbdev(interface);
+	struct usb_host_interface *altsetting = interface->cur_altsetting;
+	struct usb_endpoint_descriptor *endpoint;
+	struct usb_led *led = NULL;
+	struct device *dev = &interface->dev;
+	int ep, ep_in, ep_out;
+	int ret = -ENOMEM, size, res;
+
+	dev_info(dev, "%s() - called", __func__);
+
+	res = usb_find_last_int_out_endpoint(altsetting, &endpoint);
+	if (res) {
+		dev_info(dev, "%s() - no endpoint found", __func__);
+		return res;
+	}
+
+	ep = usb_endpoint_num(endpoint); // value from 0 to 15
+	dev_info(dev, "%s() - usb_endpoint_num() = %d [1] - endpoint number",
+		 __func__, ep);
+
+	size = usb_endpoint_maxp(endpoint);
+	dev_info(dev, "%s() - usb_endpoint_maxp() = %ld [?] - endpoint size",
+		 __func__, (long) size);
+
+	// validate endpoint and size
+	if (0 >= size) {
+		dev_info(dev, "%s() - invalid size (%d)",
+			 __func__, size);
+		return -ENODEV;
+	}
+
+	ep_in = altsetting->endpoint[0].desc.bEndpointAddress;
+	ep_out = altsetting->endpoint[1].desc.bEndpointAddress;
+
+	dev_info(dev, "%s() - endpoint IN address is 0x%02x",
+		 __func__, ep_in);
+	dev_info(dev, "%s() - endpoint OUT address is 0x%02x",
+		 __func__, ep_out);
+
+	// allocate the private data structure
+	led = kzalloc(sizeof(*led), GFP_KERNEL);
+	if (!led) {
+		dev_err(dev, "%s() - out of memory", __func__);
+		ret = -ENOMEM;
+		goto err;
+	}
+
+	// init usb_led instance
+	led->ep_in = ep_in;
+	led->ep_out = ep_out;
+
+	led->usbdev = usb_get_dev(usbdev);
+	led->intf = interface;
+
+	// alloate int_out_urb instance
+	led->interrupt_out_urb = usb_alloc_urb(0, GFP_KERNEL);
+	if (!led->interrupt_out_urb) {
+		goto err;
+	}
+
+	// initialize int_out_urb
+	usb_fill_int_urb(led->interrupt_out_urb,
+			 led->usbdev,
+			 usb_sndintpipe(led->usbdev, ep_out),
+			 (void*) &led->irq_data,
+			 64,
+			 led_urb_out_callback, led, 64);
+
+	// allocate int_in_urb instance
+	led->interrupt_in_urb = usb_alloc_urb(0, GFP_KERNEL);
+	if (!led->interrupt_in_urb) {
+		goto err;
+	}
+
+	// initialize int_in_urb
+	usb_fill_int_urb(led->interrupt_in_urb,
+			 led->usbdev,
+			 usb_rcvintpipe(led->usbdev, ep_in),
+			 (void*) &led->ibuf,
+			 64,
+			 led_urb_in_callback, led, 64);
+
+	usb_set_intfdata(interface, led);
+
+	// create a led sysfs entry to interact with user space
+	ret = device_create_file(&interface->dev, &dev_attr_led);
+	if (ret)
+		goto err_create_file;
+
+	ret = usb_submit_urb(led->interrupt_in_urb, GFP_KERNEL);
+	if (ret) {
+		dev_err(dev, "%s() - failed to submit interrupt_in_urb %d",
+			__func__, ret);
+		device_remove_file(dev, &dev_attr_led);
+		goto err_create_file;
+	}
+
+	dev_info(dev, "%s() - interrupt_in_urb submitted", __func__);
+
+	return 0;
+
+err_create_file:
+	usb_free_urb(led->interrupt_out_urb);
+	usb_free_urb(led->interrupt_in_urb);
+	usb_put_dev(usbdev);
+	usb_set_intfdata(interface, NULL);
+
+err:
+	kfree(dev);
+	return ret;
+}
+
+static void
+usbled_disconnect(struct usb_interface *interface)
+{
+	struct usb_led *led = usb_get_intfdata(interface);
+	struct device* dev = &interface->dev;
+
+	dev_info(dev, "%s() - called", __func__);
+
+	device_remove_file(&interface->dev, &dev_attr_led);
+	usb_free_urb(led->interrupt_out_urb);
+	usb_free_urb(led->interrupt_in_urb);
+	usb_set_intfdata(interface, NULL);
+	usb_put_dev(led->usbdev);
+	kfree(led);
+
+	dev_info(dev, "%s() - usb led is now disconnected", __func__);
+}
+
+/*
+  prepare the struct usb_driver to be registered at the USB core
+ */
+static struct usb_driver led_driver = {
+	.name = "lothars_usbled",
+	.id_table = usbled_id_table,
+	.probe = usbled_probe,
+	.disconnect = usbled_disconnect,
+};
+module_usb_driver(led_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Lothar Rubusch <l.rubusch@gmail.com>");
+MODULE_DESCRIPTION("pic32mx470 usb demo: led/switch usb controlled");
