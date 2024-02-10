7355940d93e97d07f9a3917bb6e6bae3a879540c
Lothar Rubusch <l.rubusch@gmail.com>
2024-02-10 20:22:01 +0100

    basics: update miscdevice demos
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ 010__basics/19__miscdevice-private-data/README.md
@@ -0,0 +1,49 @@
+# chardev driver: private data
+
+The `miscdevice` framework is an interface exported by the Linux
+kernel that allows modules to register their individual minor numbers.  
+
+The `private_data` field usually is used to hand over underlying device drivers
+or to extend a driver structure, such as here the `miscdriver` construct.  
+
+## Module
+
+Should compile cross - having `crossbuild-essentials-arm64` installed, `ARCH`, and `CROSS_COMPILE` set, execute  
+```
+$ cd ./module
+$ make
+```
+Copy the module over to the target  
+
+## Userspace
+
+Compile cross as well, set ``$CROSS_COMPILE``.   
+```
+rpi$ cd ./userspace
+rpi$ make
+```
+Copy the module over to the target  
+
+## Usage
+
+```
+# insmod ./priv_data.ko
+# echo 123 > /dev/lothars_device
+# cat /dev/lothars_device
+# rmmod priv_data
+```
+Logs  
+```
+[20:08:13.805] Feb 10 19:08:13 ctrl001 kernel: [  896.551931] mod_init(): called
+[20:08:18.482] Feb 10 19:08:18 ctrl001 kernel: [  901.227151] demo_write(): called
+[20:08:18.483] Feb 10 19:08:18 ctrl001 kernel: [  901.227199] demo_write(): received from userspace '123'
+[20:08:20.396] Feb 10 19:08:20 ctrl001 kernel: [  903.145314] demo_read(): called
+[20:08:22.473] Feb 10 19:08:22 ctrl001 kernel: [  905.238212] mod_exit(): called
+```
+
+## Verified
+* Verified against a RPI3 w/ aarch64
+
+## References
+* https://embetronicx.com/tutorials/linux/device-drivers/misc-device-driver/
+* Linux Driver Development for Embedded Procesesors, A. L. Rios, 2018
---
+++ 010__basics/19__miscdevice-private-data/priv_data.c
@@ -0,0 +1,109 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
+
+#include <linux/miscdevice.h>
+
+// a "special" device derrived from another, here, miscdevice
+#define DEV_NAME "lothars_device"
+#define DEV_MEMSIZE 256
+struct special_dev
+{
+	struct miscdevice *misc_dev;
+	char content[DEV_MEMSIZE];
+};
+
+static struct special_dev sp_dev;
+
+static int
+demo_open(struct inode *inode, struct file *file)
+{
+	file->private_data = &sp_dev;
+	return 0;
+}
+
+static ssize_t
+demo_read(struct file *file, char __user *ubuf, size_t count, loff_t *off)
+{
+	struct special_dev *dev;
+
+	pr_info("%s(): called\n", __func__);
+	dev = container_of(file->private_data, struct special_dev, misc_dev);
+	if (0 == *off) {
+		if (copy_to_user(ubuf, &dev->content, sizeof(dev->content))) {
+			pr_err("%s(): failed to return content to userspace\n",
+			       __func__);
+			return -EFAULT;
+		}
+
+		return 0;
+	}
+
+	return count;
+}
+
+static ssize_t
+demo_write(struct file *file, const char __user *ubuf,
+	   size_t count, loff_t *off)
+{
+	struct special_dev *dev;
+
+	pr_info("%s(): called\n", __func__);
+	dev = container_of(file->private_data, struct special_dev, misc_dev);
+	if (copy_from_user(dev->content, ubuf, count)) {
+		pr_err("%s(): failed to copy content\n", __func__);
+		return -EFAULT;
+	}
+	dev->content[count-1] = '\0';
+	pr_info("%s(): received from userspace '%s'\n",
+		__func__, dev->content);
+
+	return count;
+}
+
+static struct file_operations fops = {
+	.open = demo_open,
+	.read = demo_read,
+	.write = demo_write,
+};
+
+static struct miscdevice misc_dev = {
+	.name = DEV_NAME,
+	.minor = MISC_DYNAMIC_MINOR,
+	.fops = &fops,
+};
+
+static int __init mod_init(void)
+{
+	int ret = 0;
+	struct special_dev *dev;
+
+	pr_info("%s(): called\n", __func__);
+	dev = &sp_dev;
+
+	dev->misc_dev = &misc_dev;
+	ret = misc_register(dev->misc_dev);
+	if (ret) {
+		return -EINVAL;
+	}
+	return ret;
+}
+
+static void __exit mod_exit(void)
+{
+	struct special_dev *dev;
+
+	pr_info("%s(): called\n", __func__);
+	dev = &sp_dev;
+	misc_deregister(dev->misc_dev);
+}
+
+module_init(mod_init);
+module_exit(mod_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Lothar Rubusch <l.rubusch@gmail.com>");
+MODULE_DESCRIPTION("Show usage of private data of a character device.");
