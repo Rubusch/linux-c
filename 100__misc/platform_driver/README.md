c671d6a7fb3de17ebb9c917918bec184799d039e
Lothar Rubusch <l.rubusch@gmx.ch>
2024-02-14 13:10:11 +0000

    misc: add platform by driver demo
    Signed-off-by: Lothar Rubusch <l.rubusch@gmx.ch>

---
+++ 100__misc/platform_driver/README.md
@@ -0,0 +1,19 @@
+# platform driver
+
+The demo shows a dummy platform driver and a starter module. The
+platform driver usually is loaded by DT binding, alternatively by
+another module, as in this demo.  
+
+## Usage
+
+```
+$ make
+      
+
+$ dmesg
+       
+```
+
+## References:
+
+* Linux Device Driver Programming, J. Madieu, 2022
---
+++ 100__misc/platform_driver/platform-driver.c
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ */
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/miscdevice.h>
+
+#define PLATFORM_DEVNAME "lothars_device"
+#define LOTHARS_PLATFORM_DRIVER "lothars-platform-dummy"
+
+int dummy_open(struct inode * inode, struct file * filp)
+{
+	pr_info("%s(): called\n", __func__);
+	return 0;
+}
+
+int dummy_release(struct inode * inode, struct file * filp)
+{
+	pr_info("%s(): called\n", __func__);
+	return 0;
+}
+
+ssize_t dummy_read (struct file *filp, char __user * buf, size_t count,
+		    loff_t * offset)
+{
+	pr_info("%s(): called\n", __func__);
+	return 0;
+}
+
+ssize_t dummy_write(struct file * filp, const char __user * buf, size_t count,
+		    loff_t * offset)
+{
+	pr_info("%s(): called\n", __func__);
+	return count;
+}
+
+static struct file_operations fops = {
+	.open = dummy_open,
+	.release = dummy_release,
+	.read = dummy_read,
+	.write = dummy_write,
+};
+
+static struct miscdevice pf_device = {
+	.name = PLATFORM_DEVNAME,
+	.minor = MISC_DYNAMIC_MINOR,
+	.fops = &fops,
+};
+
+static int pf_probe (struct platform_device *pdev)
+{
+	pr_info("%s(): called\n", __func__);
+	if (misc_register(&pf_device)) {
+		pr_err("%s(): failed to register miscdevice\n", __func__);
+		return -EFAULT;
+	}
+	return 0;
+}
+
+static int pf_remove(struct platform_device *pdev)
+{
+	pr_info("%s(): called\n", __func__);
+	misc_deregister(&pf_device);
+	return 0;
+}
+
+static struct platform_driver pf_driver = {
+	.probe      = pf_probe,
+	.remove     = pf_remove,
+	.driver     = {
+		.name     = LOTHARS_PLATFORM_DRIVER,
+		.owner    = THIS_MODULE,
+	},
+};
+module_platform_driver(pf_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Lothar Rubusch <l.rubusch@gmail.com>");
+MODULE_DESCRIPTION("Messing with platform drivers.");
