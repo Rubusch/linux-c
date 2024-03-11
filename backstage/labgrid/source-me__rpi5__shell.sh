550c5b1f98af69c463d28c68e8d5dede25ba8d85
Lothar Rubusch <l.rubusch@gmail.com>
2024-03-11 22:42:03 +0100

    tests: environment update
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- backstage/labgrid/source-me__rpi5__shell.sh
+++ backstage/labgrid/source-me__rpi5__shell.sh
@@ -3,6 +3,7 @@
 ## cd into the labgrid folder, then source this file...
 ## labgrid-coordinator:       10.1.10.1
 ## labgrid-exporter (place):  rpi
+alias lc='labgrid-client'
 
 export LG_CROSSBAR=ws://10.1.10.1:20408/ws
 export LG_ENV=$(pwd)/rpi5__shell__env.yaml
