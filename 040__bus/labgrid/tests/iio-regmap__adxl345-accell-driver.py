54547f603cb6b884e60b91b8e5808ca9a40787c1
Lothar Rubusch <l.rubusch@gmail.com>
2024-03-19 21:09:57 +0000

    ci: bus: update numbers in labgrid tests
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- 040__bus/labgrid/tests/iio-regmap__adxl345-accell-driver.py
+++ 040__bus/labgrid/tests/iio-regmap__adxl345-accell-driver.py
@@ -12,94 +12,94 @@ import common_kernel
 from common_kernel import *
 
 
-def test_stat(cmd):
+def test_000_stat(cmd):
     do_stat(PROJECT, MODULES)
     do_stat(PROJECT, DTBOS)
 
-def test_login(shell_cmd): ## reboot
+def test_010_login(shell_cmd): ## reboot
     do_login_check(shell_cmd, KERNELVERSION)
 
-def test_copy_dt(target, cmd):
+def test_020_copy_dt(target, cmd):
     do_copy_dtbo(target, cmd, DTBOS, PROJECT)
 
-def test_modify_config_spi(cmd):
+def test_030_modify_config_spi(cmd):
     do_register_dtbo(cmd, DTBOS[IDX_SPI])
 
-def test_copy_lkm_spi(target, shell_cmd): ## reboot
+def test_040_copy_lkm_spi(target, shell_cmd): ## reboot
     do_copy_lkms(shell_cmd, target, [MODULES[0], MODULES[1]], PROJECT)
 
-def test_turn_off_wifi_spi(cmd):
+def test_050_turn_off_wifi_spi(cmd):
     cmd.run_check("sudo killall wpa_supplicant")
     cmd.run_check("sudo ip link set wlan0 down")
     cmd.run_check("sudo systemctl stop dnsmasq")
 
-def test_sudo_modprobe_industrialio01(cmd):
+def test_060_sudo_modprobe_industrialio(cmd):
     cmd.run_check("sudo modprobe industrialio")
 
-def test_sudo_modprobe_regmap_spi01(cmd):
+def test_070_sudo_modprobe_regmap_spi(cmd):
     cmd.run_check("sudo modprobe regmap_spi")
 
 ## in case some or all modules are already loaded, unload them first...
-def test_cleanup_lkm01(cmd):
+def test_080_cleanup_lkm(cmd):
     undo_load_lkms(cmd, MODULES)
 
-def test_load_spi(cmd):
+def test_090_load_spi(cmd):
     do_load_lkms(cmd, [MODULES[0], MODULES[1]]) ## spi
 
-def test_probe_spi(cmd):
+def test_100_probe_spi(cmd):
     do_log_verification(cmd, [r"adxl345_core_probe(): called",
                               r"adxl345_setup(): called",
                               r"adxl345_setup(): calling setup()",
                               r"adxl345_spi_setup(): called",
                               r"adxl345_setup(): retrieving DEVID"])
 
-def test_cleanup_lkm02(cmd):
+def test_110_cleanup_lkm(cmd):
     undo_load_lkms(cmd, MODULES)
 
-def test_revert_config02(shell_cmd): ## reboot
+def test_120_revert_config(shell_cmd): ## reboot
     undo_register_dtbo(shell_cmd)
 
 ## i2c
 
-def test_modify_config_i2c(cmd):
+def test_130_modify_config_i2c(cmd):
     do_register_dtbo(cmd, DTBOS[IDX_I2C])
 
-def test_copy_lkm_i2c(target, shell_cmd): ## reboot
+def test_140_copy_lkm_i2c(target, shell_cmd): ## reboot
     do_copy_lkms(shell_cmd, target, [MODULES[0], MODULES[2]], PROJECT)
 
-def test_turn_off_wifi_i2c(cmd):
+def test_150_turn_off_wifi_i2c(cmd):
     cmd.run_check("sudo killall wpa_supplicant")
     cmd.run_check("sudo ip link set wlan0 down")
     cmd.run_check("sudo systemctl stop dnsmasq")
 
-def test_sudo_modprobe_industrialio02(cmd):
+def test_160_sudo_modprobe_industrialio(cmd):
     cmd.run_check("sudo modprobe industrialio")
 
-def test_sudo_modprobe_i2c_dev02(cmd):
+def test_170_sudo_modprobe_i2c_dev(cmd):
     cmd.run_check("sudo modprobe i2c-dev")
 
-def test_sudo_modprobe_regmap_i2c02(cmd):
+def test_180_sudo_modprobe_regmap_i2c(cmd):
     cmd.run_check("sudo modprobe regmap_i2c")
 
 ## in case some or all modules are already loaded, unload them first...
-def test_cleanup_lkm03(cmd):
+def test_190_cleanup_lkm(cmd):
     undo_load_lkms(cmd, MODULES)
 
-def test_load_lkm_i2c(cmd):
+def test_200_load_lkm_i2c(cmd):
     do_load_lkms(cmd, [MODULES[0], MODULES[2]]) ## i2c
 
-def test_probe_i2c(cmd):
+def test_210_probe_i2c(cmd):
     do_log_verification(cmd, [r"adxl345_i2c_probe(): called",
                               r"adxl345_core_probe(): called",
                               r"adxl345_setup(): called",
                               r"adxl345_setup(): calling setup()",
                               r"adxl345_setup(): retrieving DEVID"])
 
-def test_revert_config02(shell_cmd): ## reboot
+def test_220_revert_config(shell_cmd): ## reboot
     undo_register_dtbo(shell_cmd)
 
-def test_cleanup_overlays(cmd):
+def test_230_cleanup_overlays(cmd):
     undo_copy_dtbo(cmd, DTBOS)
 
-def test_cleanup_lkm04(cmd):
+def test_240_cleanup_lkm(cmd):
     undo_load_lkms(cmd, MODULES)
