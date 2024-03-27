5854988067ab9802193fee54cdf496fe39923a5c
Lothar Rubusch <l.rubusch@gmail.com>
2024-03-27 22:38:14 +0000

    ci: labgrid: fix numbering
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- 030__platform/labgrid/tests/dt_platform_device_leds.py
+++ 030__platform/labgrid/tests/dt_platform_device_leds.py
@@ -18,21 +18,21 @@ def test_000_stat(cmd):
 def test_010_login(reboot_then_cmd): ## reboot
     do_login_check(reboot_then_cmd, KERNELVERSION)
 
-def test_031_copy_dtbo(cmd, target):
+def test_020_copy_dtbo(cmd, target):
     do_copy_dtbo(cmd, target, [DTBO], f"{PROJECT}")
 
-def test_035_regster_dtbo(cmd):
+def test_025_regster_dtbo(cmd):
     do_register_dtbo(cmd, DTBO)
 
 ## reboot
 
-def test_020_turn_off_wifi(reboot_then_cmd): ## reduce log noise
+def test_030_turn_off_wifi(reboot_then_cmd): ## reduce log noise
     cmd = reboot_then_cmd
     cmd.run_check("sudo killall wpa_supplicant")
     cmd.run_check("sudo ip link set wlan0 down")
     cmd.run_check("sudo systemctl stop dnsmasq")
 
-def test_030_copy_lkm(cmd, target):
+def test_035_copy_lkm(cmd, target):
     do_copy_files(cmd, target, MODULES, f"{PROJECT}")
 
 def test_040_load_lkm(cmd):
