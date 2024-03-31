e0615543c0ce2d4954c87f9cf615cf39dc09bec7
Lothar Rubusch <l.rubusch@gmail.com>
2024-03-31 12:51:31 +0000

    platform: modernize gpio/irq -> gpiod
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- 030__platform/gpio-interrupt__rpi4/README.md
+++ 030__platform/gpio-interrupt__rpi4/README.md
@@ -1,6 +1,8 @@
-# Hello Raspberry4 GPIO/LED
+# Hello Raspberry4 GPIO/LED with Interrupt/Button
 
-The source was tested compiled and running on 5.4.75.  
+This is basically an implementation of the project to light a led as
+described on embetronicx.com, using my own module and my own Raspi4
+build.
 
 # Build
 
@@ -11,24 +13,16 @@ $ make
 # USAGE
 
 ```
-pi@ctrl001:/tmp $ sudo insmod hello_gpio.ko 
+pi@ctrl001:/tmp $ sudo insmod hello_gpio.ko
 
-pi@ctrl001:/tmp $ sudo rmmod hello_gpio 
+pi@ctrl001:/tmp $ sudo rmmod hello_gpio
 
 pi@ctrl001:/tmp $ dmesg | tail
-    [   33.586933] hello_gpio: loading out-of-tree module taints kernel.
-    [   33.587543] mod_init() - major 239, minor 0
-    [   33.592209] gpio_irq = 186
-    [   33.592309] mod_init() done
+    [   26.846417] mindblowing_probe(): called
+    [   26.847063] mindblowing_probe(): got minor 121
+    [   26.847718] mindblowing_probe(): ok
+    [   27.025593] mindblowing_remove(): called
 ```
-
-# Notes
-
-This is basically an implementation of the project to light a led as
-described on embetronicx.com, using my own module and my own Raspi4
-build.
-
----
 
 ## References:
 
--- 030__platform/gpio-interrupt__rpi4/hello_gpio.c
+++ 030__platform/gpio-interrupt__rpi4/hello_gpio.c
@@ -4,46 +4,27 @@
   https://embetronicx.com/tutorials/linux/device-drivers/gpio-linux-device-driver-using-raspberry-pi/
 */
 
-#include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/cdev.h>
-#include <linux/uaccess.h>
-#include <linux/gpio.h>
+#include <linux/miscdevice.h>
+#include <linux/gpio/consumer.h> /* new gpio api */
+#include <linux/of_device.h>
+#include <linux/platform_device.h>
 #include <linux/interrupt.h>
-#include <linux/fs.h>
 
-/*
-  forwards
-*/
 
-static int __init mod_init(void);
-static void __exit mod_exit(void);
+#define GPIO_OUT_NAME "output"
+#define GPIO_IN_NAME "input"
 
-// device
-static ssize_t mindblowing_read(struct file *, char __user *, size_t, loff_t *);
-static ssize_t mindblowing_write(struct file *, const char __user *, size_t,
-				 loff_t *);
+#define MINDBLOWING_DEVICE_NAME "mindblowing_gpio_device"
+#define MINDBLOWING_DRIVER_NAME "mindblowing_gpio_driver"
 
-// irq
+static struct gpio_desc *mindblowing_gpio_in;
+static struct gpio_desc *mindblowing_gpio_out;
 static irqreturn_t gpio_irq_handler(int, void *);
 
-// debouncing
+// debouncing [in case prefer gpiod_debounce() ]
 extern unsigned long volatile jiffies;
 unsigned long old_jiffie = 0;
-
-/*
-  globals
-*/
-
-#define GPIO_OUT 21
-#define GPIO_IN 25
-
-#define GPIO_OUT_NAME "GPIO_21"
-#define GPIO_IN_NAME "GPIO_25"
-
-#define MINBLOWING_DEVICE_NAME "mindblowing_gpio_device"
-#define MINBLOWING_DEVICE_MINOR 123
 
 // led
 unsigned int led_toggle = 0;
@@ -51,41 +32,26 @@ unsigned int led_toggle = 0;
 // irq - needs to be global in order to free the irq at exit()
 unsigned int gpio_irq = 0;
 
-// device
-dev_t dev = 0;
-static struct class *dev_class;
-static struct cdev mindblowing_cdev;
-
-// device fops
-static struct file_operations fops = {
-	.read = mindblowing_read,
-	.write = mindblowing_write,
-};
-
-/*
-  implementation
-*/
-
 /*
   interrupt handler for GPIO 25, to be called at each risign edge
 */
 static irqreturn_t gpio_irq_handler(int irq, void *dev_id)
 {
 	static unsigned long flags = 0;
+	unsigned long diff = jiffies - old_jiffie; // debouncing
 
-	// debounce
-	unsigned long diff = jiffies - old_jiffie;
+	pr_info("%s(): called\n", __func__);
 	if (20 > diff) {
 		return IRQ_HANDLED;
 	}
 	old_jiffie = jiffies;
 
-	// make LED toggle on/off on incomming interrupt event
+	// make LED toggle on/off upon incomming interrupt event
 	local_irq_save(flags);
 	led_toggle = (0x01 ^ led_toggle);
-	gpio_set_value(GPIO_OUT, led_toggle);
-	printk(KERN_INFO "interrupt occurred - GPIO_OUT: %d\n",
-	       gpio_get_value(GPIO_OUT));
+	gpiod_set_value(mindblowing_gpio_out, led_toggle);
+	pr_info("%s(): interrupt occurred - GPIO_OUT: %d\n",
+		__func__, gpiod_get_value(mindblowing_gpio_out));
 	local_irq_restore(flags);
 
 	return IRQ_HANDLED;
@@ -95,15 +61,17 @@ static ssize_t mindblowing_read(struct file *filp, char __user *buf, size_t len,
 				loff_t *poff)
 {
 	uint8_t gpio_state = 0;
-	gpio_state = gpio_get_value(GPIO_OUT);
+
+	pr_info("%s(): called\n", __func__);
+	gpio_state = gpiod_get_value(mindblowing_gpio_out);
 
 	// write to user
 	len = 1;
 	if (copy_to_user(buf, &gpio_state, len)) {
-		printk(KERN_ERR "%s() failed\n", __func__);
+		pr_err("%s(): failed\n", __func__);
 		return -EFAULT;
 	}
-	printk(KERN_INFO "%s() - GPIO_IN: %d\n", __func__, gpio_state);
+	pr_info("%s(): GPIO_IN: %d\n", __func__, gpio_state);
 
 	return 0;
 }
@@ -112,188 +80,145 @@ static ssize_t mindblowing_write(struct file *filp, const char __user *buf,
 				 size_t len, loff_t *poff)
 {
 	uint8_t tmp_buf[10];
+
+	pr_info("%s(): called\n", __func__);
 	memset(tmp_buf, '\0', sizeof(tmp_buf));
 
 	if (copy_from_user(tmp_buf, buf, len)) {
-		printk(KERN_ERR "%s() failed\n", __func__);
+		pr_err("%s(): failed\n", __func__);
 		return -EFAULT;
 	}
-	printk(KERN_INFO "%s() - GPIO_OUT [%d] set to %c\n", __func__, GPIO_OUT,
-	       tmp_buf[0]);
+	pr_info("%s(): GPIO_OUT set to %c\n", __func__, tmp_buf[0]);
 
 	if ('1' == tmp_buf[0]) {
-		gpio_set_value(GPIO_OUT, 1);
+		gpiod_set_value(mindblowing_gpio_out, 1);
 	} else if ('0' == tmp_buf[0]) {
-		gpio_set_value(GPIO_OUT, 0);
+		gpiod_set_value(mindblowing_gpio_out, 0);
 	} else {
-		printk(KERN_ERR "%s() invalid input, set gpio to '1' or '0'\n",
+		pr_err("%s(): invalid input, set gpio to '1' or '0'\n",
 		       __func__);
 		return -EINVAL;
 	}
 	return len;
 }
 
-/*
-  init / exit
-*/
+static struct file_operations fops = {
+	.read = mindblowing_read,
+	.write = mindblowing_write,
+};
 
-static int __init mod_init(void)
+static struct miscdevice mindblowing_mdev = {
+	.name = MINDBLOWING_DEVICE_NAME,
+	.minor = MISC_DYNAMIC_MINOR,
+	.fops = &fops,
+};
+
+static int mindblowing_probe(struct platform_device* pdev)
 {
-	if (0 > alloc_chrdev_region(&dev, 0, MINBLOWING_DEVICE_MINOR,
-				    MINBLOWING_DEVICE_NAME)) {
-		printk(KERN_ERR "alloc_chrdev_region() failed\n");
-		return -ENOMEM;
-	}
-	printk(KERN_INFO "%s() - major %d, minor %d\n", __func__, MAJOR(dev),
-	       MINOR(dev));
+	int ret;
 
-	cdev_init(&mindblowing_cdev, &fops);
+	pr_info("%s(): called\n", __func__);
 
-	if (0 > cdev_add(&mindblowing_cdev, dev, 1)) {
-		printk(KERN_ERR "%s() - cdev_add() failed\n", __func__);
-		goto err_cdev;
-	}
-
-	dev_class = class_create(MINBLOWING_DEVICE_NAME);
-	if (!dev_class) {
-		printk(KERN_ERR "%s() - class_create() failed\n", __func__);
-		goto err_class;
-	}
-
-	if (!device_create(dev_class, NULL, dev, NULL,
-			   MINBLOWING_DEVICE_NAME)) {
-		printk(KERN_ERR "%s() - device_create() failed\n", __func__);
+	/* device */
+	if (misc_register(&mindblowing_mdev)) {
+		pr_err("%s(): failed to register miscdevice\n", __func__);
 		goto err_device;
 	}
+	pr_info("%s(): got minor %i\n", __func__, mindblowing_mdev.minor);
 
-	/* GPIO_OUT */
-
-	/**
-	 * "valid" GPIO numbers are nonnegative and may be passed to
-	 * setup routines like gpio_request().  only some valid numbers
-	 * can successfully be requested and used.
-	 *
-	 * Invalid GPIO numbers are useful for indicating no-such-GPIO in
-	 * platform data and other tables.
-	 */
-	if (!gpio_is_valid(GPIO_OUT)) {
-		printk(KERN_ERR "%s() - gpio %d is not valid\n", __func__,
-		       GPIO_OUT);
-		goto err_gpio_out;
+	/* gpio */
+	mindblowing_gpio_in = devm_gpiod_get(&pdev->dev, "input", GPIOD_IN);
+	ret = gpiod_direction_input(mindblowing_gpio_in);
+	if (ret) {
+		pr_err("%s(): failed to set gpio direction input\n", __func__);
+		goto err_gpio_input;
 	}
 
-	if (0 > gpio_request(GPIO_OUT, GPIO_OUT_NAME)) {
-		printk(KERN_ERR "%s() - gpio_request() failed\n", __func__);
-		goto err_gpio_out;
+	mindblowing_gpio_out = devm_gpiod_get(&pdev->dev, "output", GPIOD_OUT_LOW);
+	ret = gpiod_direction_output(mindblowing_gpio_out, 0);
+	if (ret) {
+		pr_err("%s(): failed to set gpio direction output\n", __func__);
+		goto err_gpio_output;
 	}
 
-	/**
-	 * set direction to "output"
-	 */
-	gpio_direction_output(GPIO_OUT, 0);
+	/* irq */
+	gpio_irq = gpiod_to_irq(mindblowing_gpio_in);
 
 	/**
-	 * Using this call the GPIO 21 will be visible in /sys/class/gpio/
-	 * Now you can change the gpio values by using below commands also.
-	 * # echo 1 > /sys/class/gpio/gpio21/value   (turn ON the LED)
-	 * # echo 0 > /sys/class/gpio/gpio21/value   (turn OFF the LED)
-	 * # cat /sys/class/gpio/gpio21/value        (read the value LED)
+	 * devm_request_threaded_irq - allocate an interrupt line for a managed device
+	 * @dev: device to request interrupt for
+	 * @irq: Interrupt line to allocate
+	 * @handler: Function to be called when the IRQ occurs
+	 * @thread_fn: function to be called in a threaded interrupt context. NULL
+	 *	    for devices which handle everything in @handler
+	 * @irqflags: Interrupt type flags
+	 * @devname: An ascii name for the claiming device, dev_name(dev) if NULL
+	 * @dev_id: A cookie passed back to the handler function
 	 *
-	 * the second argument prevents the direction from being changed.
-	 */
-	gpio_export(GPIO_OUT, false);
-
-	/* GPIO_IN */
-
-	if (!gpio_is_valid(GPIO_IN)) {
-		printk(KERN_ERR "%s() - gpio %d is not valid\n", __func__,
-		       GPIO_IN);
-		goto err_gpio_in;
-	}
-
-	if (0 > gpio_request(GPIO_IN, GPIO_IN_NAME)) {
-		printk(KERN_ERR "%s() - gpio_request() failed\n", __func__);
-		goto err_gpio_in;
-	}
-
-	gpio_direction_input(GPIO_IN);
-
-	// commented out, since debouncing is not supported on Raspi4
-	//	if (0 > gpio_set_debounce(GPIO_IN, 200)) {
-	//		printk(KERN_ERR "%s() - gpio_set_debounce() failed\n", __func__);
-	//		goto err_gpio_in;
-	//	}
-
-	gpio_irq = gpio_to_irq(GPIO_IN);
-	printk(KERN_INFO "gpio_irq = %d\n", gpio_irq);
-
-	/**
-	 * request_irq - Add a handler for an interrupt line
-	 * @irq:The interrupt line to allocate
-	 * @handler:Function to be called when the IRQ occurs.
-	 * Primary handler for threaded interrupts
-	 * If NULL, the default primary handler is installed
-	 * @flags:Handling flags
-	 * @name:Name of the device generating this interrupt
-	 * @dev:A cookie passed to the handler function
+	 * Except for the extra @dev argument, this function takes the
+	 * same arguments and performs the same function as
+	 * request_threaded_irq().  IRQs requested with this function will be
+	 * automatically freed on driver detach.
 	 *
-	 * This call allocates an interrupt and establishes a handler; see
-	 * the documentation for request_threaded_irq() for details.
+	 * If an IRQ allocated with this function needs to be freed
+	 * separately, devm_free_irq() must be used.
 	 *
-	 * possible flags are:
+	 * Possible flags are:
 	 * IRQF_TRIGGER_RISING
 	 * IRQF_TRIGGER_FALLING
 	 * IRQF_TRIGGER_HIGH
 	 * IRQF_TRIGGER_LOW
 	 */
-	if (request_irq(gpio_irq, (void *)gpio_irq_handler, IRQF_TRIGGER_RISING,
-			MINBLOWING_DEVICE_NAME, NULL)) {
-		printk(KERN_ERR "%s() - cannot register IRQ\n", __func__);
-		goto err_gpio_in;
+	ret = devm_request_threaded_irq(&pdev->dev,
+					gpio_irq,
+					NULL,
+					gpio_irq_handler,
+					IRQF_TRIGGER_RISING |IRQF_ONESHOT,
+					MINDBLOWING_DEVICE_NAME,
+					NULL);
+	if (ret) {
+		pr_err("%s(): cannot register IRQ\n", __func__);
+		goto err_gpio_output;
 	}
 
-	printk(KERN_INFO "%s() done\n", __func__);
+	pr_info("%s(): ok\n", __func__);
 	return 0;
 
-err_gpio_in:
-	gpio_free(GPIO_OUT);
+err_gpio_output:
+	gpiod_put(mindblowing_gpio_in);
 
-err_gpio_out:
-	device_destroy(dev_class, dev);
-
+err_gpio_input:
 err_device:
-	class_destroy(dev_class);
-
-err_class:
-	cdev_del(&mindblowing_cdev);
-
-err_cdev:
-	unregister_chrdev_region(dev, 1);
 
 	return -EFAULT;
 }
 
-static void __exit mod_exit(void)
+static int mindblowing_remove(struct platform_device *pdev)
 {
-	// irq
-	free_irq(gpio_irq, NULL);
+	pr_info("%s(): called\n", __func__);
 
-	// GPIO_IN
-	gpio_free(GPIO_IN);
+	gpiod_put(mindblowing_gpio_in);
+	gpiod_put(mindblowing_gpio_out);
+	misc_deregister(&mindblowing_mdev);
 
-	// GPIO_OUT
-	gpio_free(GPIO_OUT);
-
-	// mind-blowing device
-	device_destroy(dev_class, dev);
-	class_destroy(dev_class);
-	cdev_del(&mindblowing_cdev);
-	unregister_chrdev_region(dev, 1);
+	return 0;
 }
 
-module_init(mod_init);
-module_exit(mod_exit);
+static struct of_device_id mindblowing_match[] = {
+    {.compatible = "lothars,gpio-led"},
+    {}
+};
+
+static struct platform_driver mindblowing_driver = {
+    .probe = mindblowing_probe,
+    .remove = mindblowing_remove,
+    .driver = {
+        .name = MINDBLOWING_DRIVER_NAME,
+                .of_match_table = mindblowing_match,
+    }
+};
+module_platform_driver(mindblowing_driver);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Lothar Rubusch <l.rubusch@gmail.com>");
-MODULE_DESCRIPTION("Demonstrates a GPIO driver on RPI4!");
+MODULE_DESCRIPTION("Messing with GPIO and IRQ");
