179641e7fa98dfb9f1ee01de89aa414d685df53d
Lothar Rubusch <l.rubusch@gmail.com>
2024-04-11 22:35:27 +0000

    ci: add labgrid tests
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ 080__kthreads/000__labgrid/tests/kthread__mutex.py
@@ -0,0 +1,42 @@
+PROJECT = r"kthread__mutex"
+MODULES = ["kthread.ko"]
+KERNELVERSION = r"6.6.21"
+
+
+import sys
+## NB: this is from where pytest is called!
+sys.path.insert(0, '../../backstage/labgrid/labgrid_lib')
+import common_kernel
+from common_kernel import *
+
+
+def test_000_stat(cmd):
+    do_stat(PROJECT, MODULES)
+
+def test_010_login(cmd):
+    do_login_check(cmd, KERNELVERSION)
+
+def test_020_copy_lkm(target, cmd):
+    do_copy_files(cmd, target, MODULES, PROJECT)
+
+def test_030_turn_off_wifi(cmd): ## reduce log noise
+    cmd.run_check("sudo killall wpa_supplicant")
+    cmd.run_check("sudo ip link set wlan0 down")
+    cmd.run_check("sudo systemctl stop dnsmasq")
+
+def test_040_cleanup_lkm(cmd): ## in case already loaded, unload them first...
+    undo_load_lkms(cmd, MODULES)
+
+def test_050_load_lkm(cmd):
+    do_load_lkms(cmd, MODULES)
+
+def test_075_unload_lkm(cmd):
+    undo_load_lkms(cmd, MODULES)
+
+def test_080_logs(cmd):
+    do_dmesg_verification(cmd, ["init_hello_kernelthread() started",
+                                "init_hello_kernelthread() kernelthread initialized",
+                                "thread1 - counter = 1",
+                                "thread2 - counter = 2",
+                                "thread2 - counter = 3",
+                                "cleanup_hello_kernelthread() READY."])
